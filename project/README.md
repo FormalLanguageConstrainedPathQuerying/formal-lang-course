c28acc35f567111cba93c0786b941533054824d6
Kuznetsov Ilya Alexandrovich <55543776+kznts9v-1lya@users.noreply.github.com>
2021-09-08 16:23:36 +0300

    Task 1 is done (#1)
    * Implemented initial utilities for working with graphs
    
    * SLI is implemented and code improvements and refactoring are made
    
    * Tests fix attempt
    
    * All required changes and improvements are made
    
    * Documentation added and some improvements
    
    * Documentation added and some improvements
    
    * Documentation added and some improvements
    
    * Graph pool, get_names and other improvements are implemented, also some bugs are fixed
    
    * Graph pool, get_names and other improvements are implemented, also some bugs are fixed
    
    * Imports fixed

---
+++ project/README.md
@@ -0,0 +1,15 @@
+# GraphTools application implementation
+
+An application presenting basic graph tools now. It is in active development.
+
+## How to run GraphTools application via command line
+
+`python -m project` from the repository root.
+
+## What commands does the application support
+
+* exit
+* get_description [name: str]
+* get_two_cycles [first_cycle: int] [second_cycle: int] [first_label: str] [second_label: str]
+* save_to_dot [path: str] [name: str]: save the ever used graph by name
+* get_names: all ever used graphs list
---
+++ tests/test_graph_tools.py
@@ -0,0 +1,55 @@
+import os
+import random
+from random import randint
+
+import cfpq_data
+import networkx as nx
+
+from project import graph_tools, console_commands
+
+random.seed(19)
+
+
+def test_graph_info():
+    name = "generations"
+
+    expected_desc = graph_tools.GraphDescription(
+        name,
+        129,
+        273,
+        cfpq_data.get_labels(cfpq_data.graph_from_dataset(name, verbose=False)),
+    )
+
+    actual_desc = graph_tools.get_description(name)
+
+    assert (
+        actual_desc.nodes == expected_desc.nodes
+        and actual_desc.edges == expected_desc.edges
+        and actual_desc.edge_labels == expected_desc.edge_labels
+    )
+
+
+def test_generate_two_cycles_graph():
+    path = "tests/data/two_cycles.dot"
+
+    if os.path.exists(path):
+        os.remove(path)
+
+    desc = {
+        "first_cycle": randint(25, 49),
+        "second_cycle": randint(1, 24),
+        "edge_labels": ("one", "two"),
+    }
+
+    expected_graph = graph_tools.get_two_cycles(
+        desc["first_cycle"], desc["second_cycle"], desc["edge_labels"]
+    ).graph
+    expected_graph_pydot = str(nx.drawing.nx_pydot.to_pydot(expected_graph))
+
+    console_commands.save_graph_to_dot(path, "two_cycles")
+
+    assert os.path.exists(path)
+    with open(path, "r") as file:
+        actual_graph_pydot = file.read()
+
+        assert actual_graph_pydot == expected_graph_pydot
