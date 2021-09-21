c78383b5bb9f1f92ede003918276ca8267e5de1a
Kuznetsov Ilya Alexandrovich <55543776+kznts9v-1lya@users.noreply.github.com>
2021-09-21 10:21:20 +0300

    Task 3 NFA is done (#4)
    * Task 3 is done
    
    * Add info about function safety
    
    * CI fix attempt
    
    * Improvements in tools and more graphs in tests

--- project/__init__.py
+++ project/__init__.py
@@ -4,5 +4,5 @@ from project.console_app import *
 import project.graph_tools
 from project.graph_tools import *
 
-import project.regex_tools
-from project.regex_tools import *
+import project.automaton_tools
+from project.automaton_tools import *
