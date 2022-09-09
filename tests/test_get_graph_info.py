76caf1d38faaa09424621eb1b0cf890fe821c02f
shlegelal <shlegel.alink0@gmail.com>
2022-09-09 17:49:59 +0300

    [TASK 1] Add function returning graph information and tests

---
+++ tests/test_get_graph_info.py
@@ -0,0 +1,25 @@
+from project.utils.graph_utils import get_graph_info
+
+graph_info = get_graph_info('skos')
+
+
+def test_nodes_number():
+    assert graph_info.nodes_num == 144
+
+
+def test_edges_number():
+    assert graph_info.edges_num == 252
+
+
+def test_labels():
+    assert graph_info.labels == {'seeAlso', 'comment', 'subPropertyOf', 'rest', 'type', 'creator', 'inverseOf', 'label',
+                                 'description', 'unionOf', 'domain', 'range', 'example', 'scopeNote', 'contributor',
+                                 'title', 'subClassOf', 'definition', 'first', 'disjointWith', 'isDefinedBy'}
+
+
+def test_raise_file_not_found_error():
+    try:
+        broken = get_graph_info('aaa')
+        assert broken.nodes_num == 'forty two'
+    except FileNotFoundError:
+        assert True
