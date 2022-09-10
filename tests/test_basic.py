e30c21ae2727677ec4450626fc33850958400e66
Daniel <danielsheh02@gmail.com>
2022-09-10 19:28:00 +0300

    Refactored code style

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -15,7 +15,7 @@ def test_save_graph_in_dot():
     save_in_dot(graph_expected)
     assert filecmp.cmp(
         str(shared.ROOT) + os.sep + "output" + os.sep + "graph.dot",
-        str(shared.ROOT) + os.sep + "output" + os.sep + "expected_graph.dot"
+        str(shared.ROOT) + os.sep + "output" + os.sep + "expected_graph.dot",
     )
 
 
