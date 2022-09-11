b95b36c20c2e661a21bc8845a81519b9a5bdd772
Ivan Azarnikov <54893829+esvault@users.noreply.github.com>
2022-09-11 18:27:11 +0300

    Added documentation and tests

--- project/graph.py
+++ project/graph.py
@@ -2,8 +2,20 @@ import cfpq_data
 import networkx as nx
 
 
-def get_graph_info(name):
-    graph = cfpq_data.graph_from_csv(cfpq_data.download(name))
+def get_graph_info(name: str):
+    """Load a graph by name. Passes through it and add unique labels to set.
+
+    :return: 3-tuple (number of nodes, number of edges, all unique edge labels)
+    or None if file with passed name is not found
+
+    """
+
+    try:
+        graph_path = cfpq_data.download(name)
+        graph = cfpq_data.graph_from_csv(graph_path)
+    except FileNotFoundError:
+        print("File {name} not found".format(name=name))
+        return
 
     s = set()
     for i in dict(graph.edges).values():
@@ -13,7 +25,16 @@ def get_graph_info(name):
     return graph.number_of_nodes(), graph.number_of_edges(), s
 
 
-def two_cycles_graph_to_file(n1, n2, labels, filename):
+def two_cycles_graph_to_file(n1: int, n2: int, labels: tuple, filename: str):
+    """Generate two cycles graph and save it to dot file.
+
+    :param n1: number of nodes in first cycle
+    :param n2: number of nodes in second cycle
+    :param labels: 2-tuple, where elements mark first and second cycles of graph according
+    :param filename: name of file to save
+    :return: None
+
+    """
     g = cfpq_data.graphs.generators.labeled_two_cycles_graph(n1, n2, labels=labels)
     graph = nx.drawing.nx_pydot.to_pydot(g)
     graph.write(filename)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,8 @@
+import os
+
+import pydot
 import pytest
-import project  # on import will print something from __init__ file
+from project import graph  # on import will print something from __init__ filez
 
 
 def setup_module(module):
@@ -10,9 +13,18 @@ def teardown_module(module):
     print("basic teardown module")
 
 
-def test_1():
-    assert 1 + 1 == 2
+def test_get_graph_info():
+    (n, e, lab) = graph.get_graph_info("bzip")
+    assert n == 632 and e == 556 and lab == {"A", "D"}
 
 
-def test_2():
-    assert "1" + "1" == "11"
+def test_two_cycles_graph_to_file():
+    filename = "test.dot"
+
+    graph.two_cycles_graph_to_file(10, 14, ("H", "M"), filename)
+
+    g = pydot.graph_from_dot_file(filename)[0]
+
+    os.remove(filename)
+
+    assert len(list(g.get_nodes())) == 26
