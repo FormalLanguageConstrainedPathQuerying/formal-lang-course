d4486e53c57201ac5298e977cdcc487243168097
Andrew <a.oleyn1kov@outlook.com>
2022-09-14 23:25:54 +0300

    tests

--- project/utilities.py
+++ project/utilities.py
@@ -4,8 +4,7 @@ import networkx
 from project.graph_info import GraphInfo
 
 
-def get_graph_info(name):
-    graph = cfpq_data.graph_from_csv(cfpq_data.download(name))
+def get_graph_info(graph):
     return GraphInfo(
         graph.number_of_nodes(),
         graph.number_of_edges(),
@@ -13,6 +12,16 @@ def get_graph_info(name):
     )
 
 
-def generate_two_cycles_graph(fst_cycle_nodes_num, snd_cycle_nodes_num, labels, path):
-    graph = cfpq_data.labeled_two_cycles_graph(fst_cycle_nodes_num, snd_cycle_nodes_num, labels=labels)
+def get_graph_info_by_name(name):
+    graph = cfpq_data.graph_from_csv(cfpq_data.download(name))
+    return get_graph_info(graph)
+
+
+def generate_two_cycles_graph(num_of_fst_cycle_nodes, num_of_snd_cycle_nodes, path):
+    if num_of_fst_cycle_nodes <= 0 or num_of_snd_cycle_nodes <= 0:
+        raise ValueError("Wrong numbers of nodes in cycle(s)")
+    graph = cfpq_data.labeled_two_cycles_graph(
+        num_of_fst_cycle_nodes, num_of_snd_cycle_nodes, labels=["a", "b"]
+    )
+
     networkx.drawing.nx_pydot.write_dot(graph, path)
