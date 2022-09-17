6de6a80fdfe950506c24c1eec9ca92172fc84799
IAmFunkyFrog <st085873@student.spbu.ru>
2022-09-17 18:37:59 +0300

    Rename

--- tests/test_write_labeled_two_cycles_graph.py
+++ tests/test_write_labeled_two_cycles_graph.py
@@ -5,10 +5,12 @@ from project.graph_utils import write_labeled_two_cycles_graph_as_dot
 
 def test_write_labeled_two_cycles_graph():
     write_labeled_two_cycles_graph_as_dot(
-        (3, 3), ("a", "b"), "tests/resources/task1/actual_graph.dot"
+        (3, 3),
+        ("a", "b"),
+        "tests/resources/test_write_labeled_two_cycles_graph/actual_graph.dot",
     )
     assert filecmp.cmp(
-        "tests/resources/task1/actual_graph.dot",
-        "tests/resources/task1/expected_graph.dot",
+        "tests/resources/test_write_labeled_two_cycles_graph/actual_graph.dot",
+        "tests/resources/test_write_labeled_two_cycles_graph/expected_graph.dot",
         shallow=False,
     )
