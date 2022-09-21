6dc0a79dba2f910a529efa926f957d4dad91f677
Andrew <a.oleyn1kov@outlook.com>
2022-09-21 11:19:32 +0300

    tests

--- tests/test_task2/test_task2.py
+++ tests/test_task2/test_task2.py
@@ -1,5 +1,12 @@
+import filecmp
+
 import cfpq_data
-from pyformlang.finite_automaton import DeterministicFiniteAutomaton, EpsilonNFA
+import networkx
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    EpsilonNFA,
+    NondeterministicFiniteAutomaton,
+)
 from pyformlang.regular_expression import Regex
 
 from project.regex_utils import (
@@ -8,6 +15,10 @@ from project.regex_utils import (
     create_graph,
     create_nfa_from_graph,
 )
+
+
+def to_string(automaton):
+    return networkx.drawing.nx_pydot.to_pydot(automaton.to_networkx()).to_string()
 
 
 def test_regex_to_graph_one_letter():
@@ -58,12 +69,15 @@ def test_regex_to_graph_loop():
     assert dfa.is_equivalent_to(expected)
 
 
-def test_create_nfa_from_graph_two_gen_cycle():
+def test_create_nfa_from_graph_two_gen_cycle(tmp_path):
     graph = cfpq_data.labeled_two_cycles_graph(3, 2, labels=["a", "a"])
     automaton = create_nfa_from_graph(graph, start_states=[0], final_states=[2, 5])
 
-    assert automaton.accepts("aa")
-    assert not automaton.accepts("a")
+    path_of_expected = tmp_path / "tmp.dot"
+    with open(path_of_expected, "w") as f:
+        f.write(to_string(automaton))
+
+    assert filecmp.cmp("./tests/test_task2/expected_cyc.dot", path_of_expected)
 
 
 def test_create_nfa_from_graph():
@@ -80,4 +94,134 @@ def test_create_nfa_from_graph():
         automaton=EpsilonNFA(),
     )
 
-    assert automaton.is_equivalent_to(expected)
+    assert to_string(automaton) == to_string(expected)
+
+
+def test_create_nfa_from_empty():
+    automaton: NondeterministicFiniteAutomaton = create_nfa_from_graph(
+        graph=create_graph([], [])
+    )
+
+    expected = create_automaton(
+        transitions=[],
+        start_states=[],
+        final_states=[],
+        automaton=NondeterministicFiniteAutomaton(),
+    )
+
+    assert to_string(automaton) == to_string(expected)
+
+
+def test_regex_to_graph_one_empty():
+    dfa = regex_to_dfa(Regex(""))
+    expected = create_automaton(
+        transitions=[],
+        start_states=[],
+        final_states=[],
+        automaton=DeterministicFiniteAutomaton(),
+    )
+
+    assert to_string(dfa) == to_string(expected)
+
+
+def test_create_nfa_from_graph_1():
+    automaton = create_nfa_from_graph(
+        graph=create_graph(
+            [0, 1, 2, 3],
+            [
+                (0, "a", 1),
+                (0, "a", 2),
+                (1, "b", 2),
+                (2, "b", 3),
+                (3, "b", 0),
+                (1, "b", 1),
+            ],
+        ),
+        start_states={0},
+        final_states={1, 3},
+    )
+
+    expected = create_automaton(
+        transitions=[
+            (0, "a", 1),
+            (0, "a", 2),
+            (1, "b", 2),
+            (2, "b", 3),
+            (3, "b", 0),
+            (1, "b", 1),
+        ],
+        start_states=[0],
+        final_states=[1, 3],
+        automaton=EpsilonNFA(),
+    )
+
+    assert to_string(automaton) == to_string(expected)
+
+
+def test_create_nfa_from_graph_2():
+    automaton = create_nfa_from_graph(
+        graph=create_graph([0, 1, 2, 3], []),
+        start_states={0},
+        final_states={1, 3},
+    )
+
+    expected = create_automaton(
+        transitions=[],
+        start_states=[0],
+        final_states=[1, 3],
+        automaton=EpsilonNFA(),
+    )
+
+    assert to_string(automaton) == to_string(expected)
+
+
+def test_create_nfa_from_graph_3():
+    automaton = create_nfa_from_graph(
+        graph=create_graph(
+            [0, 1, 2, 3],
+            [
+                (0, "a", 0),
+                (0, "a", 1),
+                (0, "a", 2),
+                (0, "a", 3),
+                (1, "a", 0),
+                (1, "a", 1),
+                (1, "a", 2),
+                (1, "a", 3),
+                (2, "a", 0),
+                (2, "a", 1),
+                (2, "a", 2),
+                (2, "a", 3),
+                (3, "a", 0),
+                (3, "a", 1),
+                (3, "a", 2),
+                (3, "a", 3),
+            ],
+        ),
+    )
+
+    expected = create_automaton(
+        transitions=[
+            (0, "a", 0),
+            (0, "a", 1),
+            (0, "a", 2),
+            (0, "a", 3),
+            (1, "a", 0),
+            (1, "a", 1),
+            (1, "a", 2),
+            (1, "a", 3),
+            (2, "a", 0),
+            (2, "a", 1),
+            (2, "a", 2),
+            (2, "a", 3),
+            (3, "a", 0),
+            (3, "a", 1),
+            (3, "a", 2),
+            (3, "a", 3),
+        ],
+        start_states=[0, 1, 2, 3],
+        final_states=[0, 1, 2, 3],
+        automaton=EpsilonNFA(),
+    )
+
+    assert to_string(automaton) == to_string(expected)
