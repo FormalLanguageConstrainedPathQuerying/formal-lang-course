f7385975fe36fc7ce0089fad299c87f9e67d1195
Nikita Fast <N1kita.fast@yandex.ru>
2022-09-21 19:31:04 +0300

    исправление замечаний

--- project/graph_tools.py
+++ project/graph_tools.py
@@ -3,24 +3,6 @@ import networkx
 
 
 def load_graph(name: str):
-    """
-    Load graph by its name and returns it
-
-    Parameters
-    ----------
-    name : str
-        The name of graph
-
-    Returns
-    -------
-    networkx.MultiDiGraph
-        Loaded graph
-
-    Raises
-    ----------
-    FileNotFoundError
-        If failed to found graph with such name
-    """
     try:
         graph_path = cfpq_data.download(name)
         return cfpq_data.graph_from_csv(graph_path)
@@ -29,21 +11,6 @@ def load_graph(name: str):
 
 
 def get_graph_info(name: str):
-    """
-    Returns as tuple number of nodes, number of edges and list of all labels for graph with specified name
-
-    Parameters
-    ----------
-    name : str
-        The name of graph
-
-    Returns
-    -------
-
-    tuple
-        [num_of_nodes, num_of_edges, list_of_labels]
-    """
-
     g = load_graph(name)
 
     labels = []
@@ -54,15 +21,10 @@ def get_graph_info(name: str):
     return g.number_of_nodes(), g.number_of_edges(), labels
 
 
-def create_and_save_two_cycles_graph(n, m, labels, path):
-    """
-    Creates two cycles graph and saves it to the specified .dot file
-
-    :param n: Number of vertices in the first cycle
-    :param m: Number of vertices in the second cycle
-    :param labels: Names of labels
-    :param path: Path to file
-    :returns: None
-    """
-    g = cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
+def create_and_save_two_cycles_graph(
+    nodes_in_fst_cycle, nodes_in_snd_cycle, labels, path
+):
+    g = cfpq_data.labeled_two_cycles_graph(
+        nodes_in_fst_cycle, nodes_in_snd_cycle, labels=labels
+    )
     networkx.drawing.nx_pydot.write_dot(g, path)
