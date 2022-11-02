8a42415cd1dd06773ecb810dbd9410957b728592
ToxaKaz <anton.a.kazancev@gmail.com>
2022-11-02 20:44:46 +0300

    remove pycubool libs

--- project/__init__.py
+++ project/__init__.py
@@ -2,5 +2,3 @@ from project.utils import *
 from project.dfa_utils import *
 from project.BoolDecomposedNFA import *
 from project.rpq import *
-from project.CBBoolDecomposedNFA import *
-from project.cb_rpq import *
