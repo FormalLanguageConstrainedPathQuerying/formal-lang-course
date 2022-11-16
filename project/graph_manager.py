5e6b65fd397df4eb292c60a741bd8a7dc736e568
Andrey Tarbeev <github@tarbeev.su>
2022-11-16 21:11:30 +0300

    Add Hellings

--- project/graph_manager.py
+++ project/graph_manager.py
@@ -1,5 +1,7 @@
 from cfpq_data import *
 from networkx import MultiDiGraph, nx_pydot
+from pyformlang.cfg import Variable, CFG
+from project.cfg_manager import CFGManager
 
 
 class GraphManager:
@@ -15,6 +17,23 @@ class GraphManager:
     ) -> None:
         graph = GraphManager.__create_two_cycle_labeled_graph(sizes, labels)
         nx_pydot.write_dot(graph, path)
+
+    @staticmethod
+    def execute_cfpq(
+        cfg: CFG,
+        graph: MultiDiGraph,
+        start_symbol: Variable = Variable("S"),
+        start_nodes: set = None,
+        final_nodes: set = None,
+    ) -> set:
+        start_nodes = graph.nodes if start_nodes is None else start_nodes
+        final_nodes = graph.nodes if final_nodes is None else final_nodes
+
+        return {
+            (u, v)
+            for (N, u, v) in GraphManager.__run_hellings(cfg, graph)
+            if (N == start_symbol) and (u in start_nodes) and (v in final_nodes)
+        }
 
     @staticmethod
     def __get_graph(name: str) -> MultiDiGraph:
@@ -38,3 +57,60 @@ class GraphManager:
         return cfpq_data.labeled_two_cycles_graph(
             n=dimensions[0], m=dimensions[1], labels=labels
         )
+
+    @staticmethod
+    def __run_hellings(cfg: CFG, graph: MultiDiGraph) -> list:
+        wcnf = CFGManager.convert_cfg_to_wcnf(cfg)
+        term_productions = {
+            production for production in wcnf.productions if len(production.body) == 1
+        }
+        var_productions = {
+            production for production in wcnf.productions if len(production.body) == 2
+        }
+        eps_productions = {
+            production
+            for production in wcnf.productions
+            if len(production.body) not in (1, 2)
+        }
+
+        r1 = [
+            (production.head, start_node, end_node)
+            for production in term_productions
+            for (start_node, end_node, label) in graph.edges(data="label")
+            if label == production.body[0].value
+        ]
+        r2 = [
+            (production.head, node, node)
+            for production in eps_productions
+            for node in graph.nodes
+        ]
+        r = r1 + r2
+
+        m = r.copy()
+        while m:
+            (N, v, u) = m.pop()
+
+            for (Ni, ui, vi) in r:
+                if v == vi:
+                    for production in var_productions:
+                        closure = (production.head, ui, u)
+                        if (
+                            production.body[0] == Ni
+                            and production.body[1] == N
+                            and closure not in r
+                        ):
+                            r.append(closure)
+                            m.append(closure)
+
+            for (Ni, ui, vi) in r:
+                if u == ui:
+                    for production in var_productions:
+                        closure = (production.head, v, vi)
+                        if (
+                            production.body[0] == N
+                            and production.body[1] == Ni
+                            and closure not in r
+                        ):
+                            r.append(closure)
+                            m.append(closure)
+        return r
--- tests/test_graph_manager.py
+++ tests/test_graph_manager.py
@@ -1,9 +1,13 @@
 import filecmp
 import os
 import networkx
-from networkx import nx_pydot
 
+from networkx import nx_pydot, MultiDiGraph
 from project.graph_manager import GraphManager
+from project.cfg_manager import CFGManager
+from pyformlang.cfg import Variable
+
+path = os.path.dirname(os.path.abspath(__file__)) + "/res"
 
 
 def test_write_two_cycle_labeled_graph_to_dot():
@@ -30,3 +34,58 @@ def test_create_two_cycle_labeled_graph():
         node_match=expected.nodes,
         edge_match=expected.edges(data=True),
     )
+
+
+def test_execute_cfpq():
+    graph_edges = [
+        (0, 1, {"label": "a"}),
+        (1, 2, {"label": "a"}),
+        (2, 0, {"label": "a"}),
+        (2, 3, {"label": "b"}),
+        (3, 2, {"label": "b"}),
+    ]
+
+    expected_cfg = {(0, 3), (2, 3)}
+
+    expected_hellings = [
+        (Variable("A"), 0, 1),
+        (Variable("A"), 1, 2),
+        (Variable("A"), 2, 0),
+        (Variable("B"), 2, 3),
+        (Variable("B"), 3, 2),
+        (Variable("S"), 1, 3),
+        (Variable("C"), 1, 2),
+        (Variable("S"), 0, 2),
+        (Variable("C"), 0, 3),
+        (Variable("S"), 2, 3),
+        (Variable("C"), 2, 2),
+        (Variable("S"), 1, 2),
+        (Variable("C"), 1, 3),
+        (Variable("S"), 0, 3),
+        (Variable("C"), 0, 2),
+        (Variable("S"), 2, 2),
+        (Variable("C"), 2, 3),
+    ]
+
+    start_nodes = {0, 2}
+    final_nodes = {3}
+
+    cfg = CFGManager.read_cfg_from_file(path + "/cfg_3")
+
+    graph = MultiDiGraph()
+    graph.add_edges_from(graph_edges)
+
+    actual_cfpq = GraphManager.execute_cfpq(
+        cfg, graph, start_nodes=start_nodes, final_nodes=final_nodes
+    )
+    actual_hellings = GraphManager._GraphManager__run_hellings(cfg, graph)
+
+    assert actual_cfpq == expected_cfg
+
+    for triple in actual_hellings:
+        if triple not in expected_hellings:
+            assert False
+
+    for triple in expected_hellings:
+        if triple not in expected_hellings:
+            assert False
