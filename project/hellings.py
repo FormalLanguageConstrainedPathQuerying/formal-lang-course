e4e1f772f5cbb9d5ced83dab94ee3768e0c27c37
Nikita-Fast <N1kita.fast@yandex.ru>
2023-02-07 20:20:47 +0300

    task_9

---
+++ project/hellings.py
@@ -0,0 +1,90 @@
+from typing import Set, Tuple, Union
+
+import networkx as nx
+from pyformlang.cfg import CFG, Variable
+from project.cfg import cfg_to_wcnf
+
+
+def hellings(graph: nx.Graph, cfg: CFG) -> Set[Tuple[int, str, int]]:
+    wcnf = cfg_to_wcnf(cfg)
+
+    r = {
+        (N, v, v)
+        for v in graph.nodes
+        for N in {p.head.value for p in wcnf.productions if not p.body}
+    }.union(
+        {
+            (N, v, u)
+            for (v, u, data) in graph.edges(data=True)
+            for N in {
+                p.head.value
+                for p in wcnf.productions
+                if p.body[0].value == data["label"]
+            }
+        }
+    )
+    m = r.copy()
+
+    var_productions = {p for p in wcnf.productions if len(p.body) == 2}
+    while m != set():
+        # N_i -> v--u
+        N_i, v, u = m.pop()
+        new_triplets = set()
+
+        # пробуем пристроить путь слева к v--u
+        # N_j -> v1--v2
+        # новый путь v1--v2==v--u (в итоге v1--u)
+        for (N_j, v1, v2) in r:
+            if v2 == v:
+                for N_k in {
+                    p.head.value for p in var_productions if p.body == [N_j, N_i]
+                }:
+                    if (N_k, v1, u) not in r:
+                        new_triplets.add((N_k, v1, u))
+        m.update(new_triplets)
+        r.update(new_triplets)
+        new_triplets.clear()
+
+        # пробуем пристроить путь справа к v--u
+        # N_j -> v1--v2
+        # новый путь v--u==v1--v2 (в итоге v--v2)
+        for (N_j, v1, v2) in r:
+            if v1 == u:
+                for N_k in {
+                    p.head.value for p in var_productions if p.body == [N_i, N_j]
+                }:
+                    if (N_k, v, v2) not in r:
+                        new_triplets.add((N_k, v, v2))
+        m.update(new_triplets)
+        r.update(new_triplets)
+        new_triplets.clear()
+
+    # переупорядочиваем, чтобы соответствовать требованию в домашке
+    return {(v1, N, v2) for (N, v1, v2) in r}
+
+
+def cfpq_using_hellings(
+    graph: nx.Graph,
+    query: Union[CFG, str],
+    starts: Set[int] = None,
+    finals: Set[int] = None,
+    start_nonterminal: Union[str, Variable] = "S",
+) -> Set[Tuple[int, int]]:
+    if start_nonterminal is None:
+        start_nonterminal = "S"
+    if isinstance(start_nonterminal, str):
+        start_nonterminal = Variable(start_nonterminal)
+    if isinstance(query, str):
+        query = CFG.from_text(query, start_nonterminal)
+    if starts is None:
+        starts = graph.nodes
+    if finals is None:
+        finals = graph.nodes
+
+    data = hellings(graph, query)
+
+    return {
+        (v1, v2)
+        for (v1, N, v2) in data
+        if (v1 in starts and v2 in finals and N == start_nonterminal)
+    }
