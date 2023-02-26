3e587000eff9fab42faf1941233480849d56ced0
Andrei Egorychev <andrei1098765432@mail.ru>
2023-02-26 22:20:26 +0300

    fix task 1

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -1,9 +1,13 @@
 import cfpq_data
+import networkx
+import pydot
 
 
 def get_graph(name):
     return cfpq_data.graph_from_csv(cfpq_data.download(name))
 
 
-def get_labeled_two_cycle_graph(n, m, labels=("a", "b")):
-    return cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
+def save_labeled_two_cycle_graph(path, n, m, labels=("a", "b")):
+    graph = cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
+    networkx.drawing.nx_pydot.write_dot(graph, path)
+    return graph
--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -1,6 +1,5 @@
 import pytest
-
-# on import will print something from __init__ file
+import pydot
 import project.graph_utils as utils
 
 
@@ -22,15 +21,27 @@ def test_get_graph():
     assert graph.number_of_nodes() == 632
 
 
-def test_get_labeled_two_cycle_graph():
-    graph = utils.get_labeled_two_cycle_graph(5, 5)
+def test_save_labeled_two_cycle_graph():
+    PATH = "/tmp/graph"
+
+    graph = utils.save_labeled_two_cycle_graph(PATH + "1", 5, 5)
     assert graph.number_of_edges() == 12
     assert graph.number_of_nodes() == 11
     assert list(graph.edges(data="label"))[0][2] == "a"
     assert list(graph.edges(data="label"))[10][2] == "b"
 
-    graph = utils.get_labeled_two_cycle_graph(10, 4, ("labe1 1", "label 2"))
+    pd_graph = pydot.graph_from_dot_file(PATH + "1")[0]
+    assert len(pd_graph.get_nodes()) - 1 == graph.number_of_nodes()
+    assert len(pd_graph.get_edges()) == graph.number_of_edges()
+
+    graph = utils.save_labeled_two_cycle_graph(
+        PATH + "2", 10, 4, ("labe1 1", "label 2")
+    )
     assert graph.number_of_edges() == 16
     assert graph.number_of_nodes() == 15
     assert list(graph.edges(data="label"))[0][2] == "labe1 1"
     assert list(graph.edges(data="label"))[12][2] == "label 2"
+
+    pd_graph = pydot.graph_from_dot_file(PATH + "2")[0]
+    assert len(pd_graph.get_nodes()) - 1 == graph.number_of_nodes()
+    assert len(pd_graph.get_edges()) == graph.number_of_edges()
