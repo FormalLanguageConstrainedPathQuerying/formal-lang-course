4129af4908bf6d57a3622c2159691bda67dd3856
Anton Surkis <antonsurkis@gmail.com>
2023-02-28 11:55:16 +0300

    [no ci] Add functions for getting graphs

--- project/graphs.py
+++ project/graphs.py
@@ -5,13 +5,27 @@ from collections import namedtuple
 GraphInfo = namedtuple("GraphInfo", ["number_of_nodes", "number_of_edges", "edges"])
 
 
-def get_graph_info(name: str) -> GraphInfo:
+def get_graph_by_name(name: str) -> nx.MultiDiGraph:
     path = cfpq.download(name)
     g = cfpq.graph_from_csv(path)
-    return GraphInfo(g.number_of_nodes(), g.number_of_edges(), g.edges(data=True))
+    return g
+
+
+def make_two_cycles(n: int, m: int, labels=("a", "b")):
+    return cfpq.labeled_two_cycles_graph(n=n, m=m, labels=labels)
+
+
+def get_graph_info(name: str) -> GraphInfo:
+    g = get_graph_by_name(name)
+    gi = GraphInfo(g.number_of_nodes(), g.number_of_edges(), g.edges(data=True))
+    return gi
+
+
+def save_graph(path: str, g: nx.MultiDiGraph):
+    dot = nx.drawing.nx_pydot.to_pydot(g)
+    dot.write_raw(path)
 
 
 def save_two_cycles(path: str, n: int, m: int, labels=("a", "b")):
-    g = cfpq.labeled_two_cycles_graph(n=n, m=m, labels=labels)
-    dot = nx.drawing.nx_pydot.to_pydot(g)
-    dot.write_raw(path)
+    g = make_two_cycles(n=n, m=m, labels=labels)
+    save_graph(path, g)
