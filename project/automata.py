971f72550e0cebb82e8a85720eb576429018971f
PinochetLab <a.vorobev1@g,nsu.ru>
2023-03-01 16:15:04 +0300

    made branch

---
+++ project/automata.py
@@ -0,0 +1,11 @@
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+
+def regex_to_dka(regex):
+
+    pass
+
+
+def graph_to_nka(graph):
+
+    pass
