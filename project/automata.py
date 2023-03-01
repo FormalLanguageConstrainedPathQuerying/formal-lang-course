6e481f40da9ce2e4dbc6987863bff4be976309c7
PinochetLab <a.vorobev1@g,nsu.ru>
2023-03-01 19:45:38 +0300

    made automata tests

--- project/automata.py
+++ project/automata.py
@@ -1,11 +1,19 @@
-from pyformlang.regular_expression import Regex
-from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton as DFA
+from pyformlang.finite_automaton import Symbol
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton as NFA
+
 
 def regex_to_dka(regex):
+    return regex.to_epsilon_nfa().minimize()
 
-    pass
 
-
-def graph_to_nka(graph):
-
-    pass
+def graph_to_nka(graph, bs, es):
+    res = NFA(graph)
+    for b in bs:
+        res.add_start_state(b)
+    for e in es:
+        res.add_final_state(e)
+    for v, u, data in graph.edges(data=True):
+        print(data['label'])
+        res.add_transition(v, Symbol(data['label']), u)
+    return res
---
+++ tests/test_automata.py
@@ -0,0 +1,72 @@
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import State
+from project.automata import *
+from networkx import MultiDiGraph as Graph
+
+
+def test_regex_to_dka():
+    regex = Regex("(0|1)*0 0 1*")
+    dka = regex_to_dka(regex)
+    real = DFA()
+
+    s0 = State(0)
+    s1 = State(1)
+    s2 = State(2)
+    s3 = State(3)
+
+    real.add_start_state(s0)
+
+    real.add_final_state(s2)
+    real.add_final_state(s3)
+
+    O = Symbol("0")
+    I = Symbol("1")
+
+    real.add_transition(s0, I, s0)
+    real.add_transition(s0, O, s1)
+    real.add_transition(s1, I, s0)
+    real.add_transition(s1, O, s2)
+    real.add_transition(s2, O, s2)
+    real.add_transition(s3, O, s1)
+    real.add_transition(s2, I, s3)
+    real.add_transition(s3, I, s3)
+
+    assert real.is_equivalent_to(dka)
+    assert True
+
+
+def test_graph_to_nka():
+    g = Graph()
+
+    g.add_node(0)
+    g.add_node(1)
+    g.add_node(2)
+    g.add_node(3)
+
+    g.add_edge(0, 1, label='01')
+    g.add_edge(0, 0, label='00')
+    g.add_edge(1, 2, label='12')
+    g.add_edge(1, 3, label='13')
+    g.add_edge(3, 2, label='32')
+
+    real = NFA()
+
+    s0 = State(0)
+    s1 = State(1)
+    s2 = State(2)
+    s3 = State(3)
+
+    real.add_start_state(s0)
+
+    real.add_final_state(s2)
+    real.add_final_state(s3)
+
+    real.add_transition(s0, Symbol('01'), s1)
+    real.add_transition(s0, Symbol('00'), s0)
+    real.add_transition(s1, Symbol('12'), s2)
+    real.add_transition(s1, Symbol('13'), s3)
+    real.add_transition(s3, Symbol('32'), s2)
+
+    nka = graph_to_nka(g, [0], [2, 3])
+
+    assert nka.is_equivalent_to(real)
