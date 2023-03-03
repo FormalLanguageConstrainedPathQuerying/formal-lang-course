91b09a64957c16008dac037e10608630ce71d210
Nikolai <nikolai.stoiko@yandex.ru>
2023-03-03 11:39:22 +0300

    Add Docstrings

--- project/finite_automata_converters.py
+++ project/finite_automata_converters.py
@@ -8,9 +8,25 @@ from pyformlang.finite_automaton import Symbol, Epsilon
 
 
 class FAConverters(object):
+    """Class FAConverters transforms regex to DFA, graph to NFA.
+
+    Methods
+    -------
+    regex_to_min_dfa(regex_str)
+        Convert regex string to minimum DeterministicFiniteAutomaton.
+
+    graph_to_nfa(cls, graph: MultiDiGraph, start_nodes: set = None, finish_nodes: set = None)
+        From MultiDiGraph and sets of start and finish nodes builds NondeterministicFiniteAutomaton.
+    """
+
     @classmethod
-    def regex_to_min_dfa(cls, regex_str) -> DeterministicFiniteAutomaton:
-        # Convert regex string to minimum DeterministicFiniteAutomaton
+    def regex_to_min_dfa(cls, regex_str: str) -> DeterministicFiniteAutomaton:
+        """
+
+        Convert regex string to minimum DeterministicFiniteAutomaton.
+        @type regex_str: string with regex.
+        @rtype: DeterministicFiniteAutomaton which is build with regex_str.
+        """
         regex = Regex(regex_str)
         nfa: EpsilonNFA = regex.to_epsilon_nfa()
         return nfa.minimize()
@@ -19,7 +35,14 @@ class FAConverters(object):
     def graph_to_nfa(
         cls, graph: MultiDiGraph, start_nodes: set = None, finish_nodes: set = None
     ) -> NondeterministicFiniteAutomaton:
-        # From MultiDiGraph and sets of start and finish nodes builds NondeterministicFiniteAutomaton
+        """
+
+        From MultiDiGraph and sets of start and finish nodes builds NondeterministicFiniteAutomaton.
+        @param graph: input graph for transforming.
+        @param start_nodes: set of nodes, which should be start. If is None all nodes will be start.
+        @param finish_nodes: set of nodes, which should be finish. If is None all nodes will be finish.
+        @return: NondeterministicFiniteAutomaton which is build from graph.
+        """
         if finish_nodes is None:
             finish_nodes = set(graph.nodes)
         if start_nodes is None:
