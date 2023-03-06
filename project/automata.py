85983fbc820724f835f872a4a57fdd2032d6d918
PinochetLab <a.vorobev1@g,nsu.ru>
2023-03-06 18:37:18 +0300

    added doc strings

--- project/automata.py
+++ project/automata.py
@@ -3,10 +3,12 @@ from pyformlang.finite_automaton import Symbol
 from pyformlang.finite_automaton import NondeterministicFiniteAutomaton as NFA
 
 
+# Creates Deterministic Finite Automata from Regex
 def regex_to_dka(regex):
     return regex.to_epsilon_nfa().minimize()
 
 
+# Creates Nondeterministic Automata from MultiDiGraph, begin nodes list, end nodes list
 def graph_to_nka(graph, bs, es):
     res = NFA(graph)
     for b in bs:
