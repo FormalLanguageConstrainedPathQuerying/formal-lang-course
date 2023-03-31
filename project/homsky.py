b614f50f4345cc9b4aa5801ec532e21027cccf71
PinochetLab <a.vorobev1@g,nsu.ru>
2023-03-31 20:33:52 +0300

    made homsky and tests

---
+++ project/homsky.py
@@ -0,0 +1,71 @@
+import cfpq_data as cfpq
+import networkx as nx
+from collections import namedtuple
+from pyformlang.cfg import CFG
+
+# graph info
+GI = namedtuple("GI", ["n_c", "e_c", "ls"])
+
+
+def info(g: nx.Graph) -> GI:
+    """
+    Returns an info about graph
+    :param g: nx.Graph graph
+    :return: GI
+    """
+    ls_s = set(ats["label"] for (a, b, ats) in g.edges.data())
+    return GI(len(g.nodes), len(g.edges), list(ls_s))
+
+
+def from_dataset(path: str) -> GI:
+    """
+    Returns graph info from csv on path
+    :param path: path to csv file
+    :return: GI
+    """
+    return info(cfpq.graph_from_csv(cfpq.download(path)))
+
+
+def save(g: nx.Graph, path: str):
+    """
+    Saves graph to path
+    :param g: nx.Graph graph
+    :param path: path to save
+    :return: void
+    """
+    nx.drawing.nx_pydot.to_pydot(g).write_raw(path)
+
+
+def two_cycled_graph_again_and_again(fp: str, fl: str, fn: int, sn: int, sl: str):
+    """
+    Creates two cycled graph with provided parameters and writes it to path
+    :param fp: file path
+    :param fl: label of edges in first cycle
+    :param fn: number of nodes in first cycle
+    :param sn: number of nodes in second cycle
+    :param sl: label of edges in second cycle
+    :return: void
+    """
+    save(cfpq.labeled_two_cycles_graph(fn, sn, labels=(fl, sl)), fp)
+
+
+def parse(p: str) -> CFG:
+    """
+    Parses CFG
+    :param p: file path
+    :return: CFG
+    """
+    with open(p, "r") as f:
+        ct = f.read()
+        return CFG.from_text(ct)
+
+
+def to_homsky_form(cfg: CFG) -> CFG:
+    """
+    Converts CFG to weak Homsky form
+    :param cfg: CFG
+    :return: CFG
+    """
+    nf = cfg.remove_useless_symbols().eliminate_unit_productions().remove_useless_symbols()
+    w = set(nf._decompose_productions(nf._get_productions_with_only_single_terminals()))
+    return CFG(start_symbol=nf.start_symbol, productions=w)
---
+++ tests/test_homsky.py
@@ -0,0 +1,66 @@
+import filecmp
+import os.path
+
+import cfpq_data as cfpq
+import pytest
+from pyformlang.cfg import CFG, Variable, Production, Terminal
+
+from project.homsky import *
+
+
+def test_info():
+    tc = cfpq.labeled_two_cycles_graph(14, 87, labels=("a", "b"))
+    i = info(tc)
+    assert i.n_c == len(tc.nodes)
+    assert i.e_c == len(tc.edges)
+    assert set(i.ls) == {"a", "b"}
+
+
+def test_from_dataset():
+    i = from_dataset("bzip")
+    assert i.n_c == 632
+    assert i.e_c == 556
+    assert len(set(i.ls)) == 2
+
+
+def assertion(c1, c2):
+    assert c1.start_symbol == c2.start_symbol
+    assert c1.productions == c2.productions
+
+
+def test_homsky():
+    s = CFG.from_text("S -> 0 1 1")
+    n = to_homsky_form(s)
+    assert set(CFG.to_text(n).removesuffix('\n').split('\n')) \
+           == {'1#CNF# -> 1', 'C#CNF#1 -> "VAR:1#CNF#" "VAR:1#CNF#"', '0#CNF# -> 0', 'S -> "VAR:0#CNF#" C#CNF#1'}
+
+    s = CFG.from_text("S -> 0 1")
+    n = to_homsky_form(s)
+    assert set(CFG.to_text(n).removesuffix('\n').split('\n')) \
+           == {'1#CNF# -> 1', 'S -> "VAR:0#CNF#" "VAR:1#CNF#"', '0#CNF# -> 0'}
+
+    s = CFG.from_text("S -> 0 0")
+    n = to_homsky_form(s)
+    assert set(CFG.to_text(n).removesuffix('\n').split('\n')) \
+           == {'0#CNF# -> 0', 'S -> "VAR:0#CNF#" "VAR:0#CNF#"'}
+
+    s = CFG.from_text("S -> 1")
+    n = to_homsky_form(s)
+    assert set(CFG.to_text(n).removesuffix('\n').split('\n')) \
+           == {'S -> 1'}
+
+    s = CFG.from_text("S -> epsilon")
+    n = to_homsky_form(s)
+    assert set(CFG.to_text(n).removesuffix('\n').split('\n')) \
+           == {'S -> '}
+
+    s = CFG.from_text("S -> epsilon 0 1")
+    n = to_homsky_form(s)
+    assert set(CFG.to_text(n).removesuffix('\n').split('\n')) \
+           == {'S -> "VAR:0#CNF#" "VAR:1#CNF#"', '0#CNF# -> 0', '1#CNF# -> 1'}
+
+    s = CFG.from_text("S -> A S B | c d\nB -> b\nA -> a")
+    n = to_homsky_form(s)
+    assert set(CFG.to_text(n).removesuffix('\n').split('\n')) \
+           == {'S -> "VAR:c#CNF#" "VAR:d#CNF#"', 'B -> b', 'S -> A C#CNF#1', 'A -> a', 'C#CNF#1 -> S B', 'c#CNF# -> c',
+               'd#CNF# -> d'}
