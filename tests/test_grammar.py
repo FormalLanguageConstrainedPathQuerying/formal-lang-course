e4e7149c42ae44b1c088991e7f88f0cd537a9b98
Nikolai <nikolai.stoiko@yandex.ru>
2023-04-01 19:56:17 +0300

    Try to fix test failed on windows, delete=False

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -17,89 +17,90 @@ def teardown_module(module):
 
 def test_1_cfg_from_file_empty():
     with tempfile.NamedTemporaryFile(
-        mode="w", dir=os.path.dirname(os.path.realpath(__file__))
+        mode="w", dir=os.path.dirname(os.path.realpath(__file__)), delete=False
     ) as tmp:
         tmp.writelines([])
-        cfg = cfg_from_file(tmp.name)
-        assert cfg.is_empty()
+        filename = tmp.name
+    cfg = cfg_from_file(filename)
+    assert cfg.is_empty()
 
     print("cfg_from_file_empty test asserted")
 
 
 def test_2_cfg_from_file():
     with tempfile.NamedTemporaryFile(
-        mode="w", dir=os.path.dirname(os.path.realpath(__file__))
+        mode="w", dir=os.path.dirname(os.path.realpath(__file__)), delete=False
     ) as tmp:
         tmp.write("\n".join(["S -> A | B a", "A -> a", "B -> b"]))
-        tmp.flush()
-        cfg = cfg_from_file(tmp.name)
-        assert not cfg.is_empty()
-        assert len(cfg.productions) == 4
-        assert (
-            pfl_cfg.Production(pfl_cfg.Variable("A"), [pfl_cfg.Terminal("a")])
-            in cfg.productions
+        filename = tmp.name
+    cfg = cfg_from_file(filename)
+    assert not cfg.is_empty()
+    assert len(cfg.productions) == 4
+    assert (
+        pfl_cfg.Production(pfl_cfg.Variable("A"), [pfl_cfg.Terminal("a")])
+        in cfg.productions
+    )
+    assert (
+        pfl_cfg.Production(pfl_cfg.Variable("S"), [pfl_cfg.Variable("A")])
+        in cfg.productions
+    )
+    assert (
+        pfl_cfg.Production(
+            pfl_cfg.Variable("S"), [pfl_cfg.Variable("B"), pfl_cfg.Terminal("a")]
         )
-        assert (
-            pfl_cfg.Production(pfl_cfg.Variable("S"), [pfl_cfg.Variable("A")])
-            in cfg.productions
-        )
-        assert (
-            pfl_cfg.Production(
-                pfl_cfg.Variable("S"), [pfl_cfg.Variable("B"), pfl_cfg.Terminal("a")]
-            )
-            in cfg.productions
-        )
-        assert (
-            pfl_cfg.Production(pfl_cfg.Variable("S"), [pfl_cfg.Terminal("b")])
-            not in cfg.productions
-        )
+        in cfg.productions
+    )
+    assert (
+        pfl_cfg.Production(pfl_cfg.Variable("S"), [pfl_cfg.Terminal("b")])
+        not in cfg.productions
+    )
     print("cfg_from_file test asserted")
 
 
 def test_3_cfg_to_weak_cnf():
     with tempfile.NamedTemporaryFile(
-        mode="w", dir=os.path.dirname(os.path.realpath(__file__))
+        mode="w", dir=os.path.dirname(os.path.realpath(__file__)), delete=False
     ) as tmp:
         tmp.write("\n".join(["S -> $ | A | B a", "A -> a | C", "B -> b", "D -> C"]))
-        tmp.flush()
-        cfg = cfg_from_file(tmp.name)
-        weak_cnf = cfg_to_weak_cnf(cfg)
-        assert not weak_cnf.is_empty()
-        assert weak_cnf.variables == {
-            pfl_cfg.Variable("S"),
-            pfl_cfg.Variable("B"),
-            pfl_cfg.Variable("a#CNF#"),
-        }
-        assert weak_cnf.contains("")
-        assert weak_cnf.contains("a")
-        assert weak_cnf.contains("ba")
+        filename = tmp.name
+    cfg = cfg_from_file(filename)
+    weak_cnf = cfg_to_weak_cnf(cfg)
+    assert not weak_cnf.is_empty()
+    assert weak_cnf.variables == {
+        pfl_cfg.Variable("S"),
+        pfl_cfg.Variable("B"),
+        pfl_cfg.Variable("a#CNF#"),
+    }
+    assert weak_cnf.contains("")
+    assert weak_cnf.contains("a")
+    assert weak_cnf.contains("ba")
     print("cfg_to_weak_cnf test asserted")
 
 
 def test_4_cfg_to_weak_cnf_with_start():
     with tempfile.NamedTemporaryFile(
-        mode="w", dir=os.path.dirname(os.path.realpath(__file__))
+        mode="w", dir=os.path.dirname(os.path.realpath(__file__)), delete=False
     ) as tmp:
         tmp.write(
             "\n".join(
                 ["F -> A | B a | C", "A -> a | C B | $", "B -> b", "D -> C", "C -> b c"]
             )
         )
-        tmp.flush()
-        cfg = cfg_from_file(tmp.name, "F")
-        weak_cnf = cfg_to_weak_cnf(cfg)
-        assert not weak_cnf.is_empty()
-        assert weak_cnf.variables == {
-            pfl_cfg.Variable("F"),
-            pfl_cfg.Variable("B"),
-            pfl_cfg.Variable("C"),
-            pfl_cfg.Variable("a#CNF#"),
-            pfl_cfg.Variable("b#CNF#"),
-            pfl_cfg.Variable("c#CNF#"),
-        }
-        assert weak_cnf.contains("")
-        assert weak_cnf.contains("a")
-        assert weak_cnf.contains("ba")
-        assert weak_cnf.contains("bc")
-        assert weak_cnf.contains("bcb")
+        filename = tmp.name
+    cfg = cfg_from_file(filename, "F")
+    weak_cnf = cfg_to_weak_cnf(cfg)
+    assert not weak_cnf.is_empty()
+    assert weak_cnf.variables == {
+        pfl_cfg.Variable("F"),
+        pfl_cfg.Variable("B"),
+        pfl_cfg.Variable("C"),
+        pfl_cfg.Variable("a#CNF#"),
+        pfl_cfg.Variable("b#CNF#"),
+        pfl_cfg.Variable("c#CNF#"),
+    }
+    assert weak_cnf.contains("")
+    assert weak_cnf.contains("a")
+    assert weak_cnf.contains("ba")
+    assert weak_cnf.contains("bc")
+    assert weak_cnf.contains("bcb")
     print("cfg_to_weak_cnf_with_start test asserted")
