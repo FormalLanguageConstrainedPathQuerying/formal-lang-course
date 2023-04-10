9fd20412226f8be4c8ce024eedfd483ce5260ff7
grigory <gregra@mail.ru>
2023-04-11 02:48:39 +0300

    Fix typing

--- project/rfa.py
+++ project/rfa.py
@@ -1,10 +1,11 @@
 from pyformlang.cfg import CFG, Variable
 from pyformlang.finite_automaton import EpsilonNFA
 from project.graph_regular_query import fa2matrix
+from typing import Dict
 
 
 class RFA:
-    def __init__(self, start_symbol, dfas: dict[Variable, EpsilonNFA]):
+    def __init__(self, start_symbol: Variable, dfas: Dict[Variable, EpsilonNFA]):
         self.start_symbol = start_symbol
         self.dfas = dfas
 
