f1b392442e99380f3cad65cadfa3a4ad6cab5523
Nikolai <nikolai.stoiko@yandex.ru>
2023-04-17 23:56:12 +0300

    Add different sources for Hellings

--- project/hellings.py
+++ project/hellings.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 from networkx import MultiDiGraph
 from pyformlang.cfg import CFG
 from pyformlang.cfg import Variable
 from pyformlang.cfg import Terminal
-from project.grammar import cfg_to_weak_cnf
+from project.grammar import cfg_to_weak_cnf, cfg_from_file
+from project.graph_utils import GraphUtils
 
 
 def hellings(cfg: CFG, graph: MultiDiGraph) -> set[tuple[object, Variable, object]]:
@@ -51,3 +53,29 @@ def hellings(cfg: CFG, graph: MultiDiGraph) -> set[tuple[object, Variable, objec
         res |= buf
 
     return res
+
+
+def hellings_graph_from_file(
+    graph_filename: Path, cfg: CFG
+) -> set[tuple[object, Variable, object]]:
+    return hellings(cfg, GraphUtils.open_graph(graph_filename))
+
+
+def hellings_cfg_from_text(
+    graph: MultiDiGraph, cfg_text: str, start_symbol=Variable("S")
+) -> set[tuple[object, Variable, object]]:
+    return hellings(CFG.from_text(cfg_text, start_symbol=start_symbol), graph)
+
+
+def hellings_cfg_from_file(
+    graph: MultiDiGraph, cfg_filename: str, start_symbol="S"
+) -> set[tuple[object, Variable, object]]:
+    return hellings(cfg_from_file(cfg_filename, start_symbol), graph)
+
+
+def hellings_cfg_and_graph_from_file(
+    graph_filename: Path, cfg_filename: str, start_symbol="S"
+) -> set[tuple[object, Variable, object]]:
+    return hellings_cfg_from_file(
+        GraphUtils.open_graph(graph_filename), cfg_filename, start_symbol
+    )
