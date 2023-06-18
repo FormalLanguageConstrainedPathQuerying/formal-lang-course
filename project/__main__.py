362468b2ba3319678465b569de6e3809414d467e
Nikolai <nikolai.stoiko@yandex.ru>
2023-06-19 01:40:29 +0300

    Add main

--- project/__main__.py
+++ project/__main__.py
@@ -1,1 +1,16 @@
-print("exec sources directory")
+import sys
+from antlr4 import *
+
+from project.parser.MyGQLLexer import MyGQLLexer
+from project.parser.MyGQLParser import MyGQLParser
+
+from project.interpreter import interpret
+
+
+def main(args):
+    parser = MyGQLParser(CommonTokenStream(MyGQLLexer(StdinStream(encoding="utf-8"))))
+    interpret(parser.prog(), sys.stdout)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
