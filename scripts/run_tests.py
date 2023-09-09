80833a057d7acad220c683a04d24730924dc8de0
Azim Muradov <azim.muradov.dev@gmail.com>
2023-09-09 19:30:46 +0300

    Add some tests and coverage check

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -4,7 +4,21 @@ import shared
 
 def main():
     shared.configure_python_path()
-    subprocess.check_call(["python", "-m", "pytest", "-vv", "-s", shared.TESTS])
+    subprocess.check_call(
+        [
+            "python",
+            "-m",
+            "pytest",
+            "-vv",
+            "-s",
+            "--cov=project",
+            "--no-cov-on-fail",
+            "--cov-branch",
+            # "--cov-report=html", # Uncomment if you html reports
+            "--cov-fail-under=50",
+            shared.TESTS,
+        ]
+    )
 
 
 if __name__ == "__main__":
--- tests/test_cfpq_utils.py
+++ tests/test_cfpq_utils.py
@@ -1,7 +1,5 @@
-import pytest
-from networkx import Graph
+from networkx import Graph, MultiDiGraph
 
-import project  # on import will print something from __init__ file
 from project.cfpq.graph_info import GraphInfo
 from project.cfpq.utils import get_graph_info
 
@@ -17,4 +15,24 @@ def teardown_module(module):
 def test_get_graph_info_empty_graph():
     graph = Graph()
     graph_info = GraphInfo()
+
+    assert graph_info == get_graph_info(graph)
+
+
+def test_get_graph_info_nonempty_graph():
+    graph = MultiDiGraph()
+    graph.add_node(1)
+    graph.add_node(2)
+    graph.add_node(3)
+    graph.add_node(4)
+    graph.add_node(5)
+    graph.add_edge(1, 3, label="a")
+    graph.add_edge(2, 3, label="b")
+    graph.add_edge(4, 1, label="a")
+    graph.add_edge(1, 4, label="c")
+
+    graph_info = GraphInfo(
+        number_of_nodes=5, number_of_edges=4, unique_labels={"a", "b", "c"}
+    )
+
     assert graph_info == get_graph_info(graph)
