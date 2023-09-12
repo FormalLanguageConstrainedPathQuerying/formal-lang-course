8e985195ae08fe085f144531179b2e724af4b69e
Danila Pechenev <d.pechenev@yadro.com>
2023-09-13 01:50:53 +0300

    Apply suggestion on static functions

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Union, Tuple, Any
 from collections.abc import Iterable
@@ -13,30 +12,24 @@ class GraphInfo:
     labels_set: set
 
 
-class GraphUtils(ABC):
-    @abstractmethod
-    def __init__(self) -> None:
-        super().__init__()
+def get_graph_info(graph_name: str) -> GraphInfo:
+    graph = cfpq_data.graph_from_csv(cfpq_data.download(graph_name))
+    return GraphInfo(
+        graph.number_of_nodes(),
+        graph.number_of_edges(),
+        set(cfpq_data.get_sorted_labels(graph)),
+    )
 
-    @staticmethod
-    def get_graph_info(graph_name: str) -> GraphInfo:
-        graph = cfpq_data.graph_from_csv(cfpq_data.download(graph_name))
-        return GraphInfo(
-            graph.number_of_nodes(),
-            graph.number_of_edges(),
-            set(cfpq_data.get_sorted_labels(graph)),
-        )
 
-    @staticmethod
-    def save_labeled_two_cycles_graph(
-        n: Union[int, Iterable[Any]],
-        m: Union[int, Iterable[Any]],
-        common_node: Union[int, Any],
-        path: str,
-        labels: Tuple[str, str] = ("a", "b"),
-    ) -> None:
-        # pylint: disable=E1101
-        graph = cfpq_data.labeled_two_cycles_graph(
-            n, m, common_node=common_node, labels=labels
-        )
-        to_pydot(graph).write_raw(path)
+def save_labeled_two_cycles_graph(
+    n: Union[int, Iterable[Any]],
+    m: Union[int, Iterable[Any]],
+    common_node: Union[int, Any],
+    path: str,
+    labels: Tuple[str, str] = ("a", "b"),
+) -> None:
+    # pylint: disable = E1101
+    graph = cfpq_data.labeled_two_cycles_graph(
+        n, m, common_node=common_node, labels=labels
+    )
+    to_pydot(graph).write_raw(path)
