2c6fdec41bc6c2a194035530f0668fdfbd26ec66
Naniduan <41004075+Naniduan@users.noreply.github.com>
2023-09-14 02:34:31 +0300

    run style formatter

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -4,8 +4,7 @@ import pydot
 __all__ = ["get_graph_stats", "generate_two_cycles_graph"]
 
 
-def get_graph_stats(
-        graph_name):
+def get_graph_stats(graph_name):
     graph_path = cfpq_data.download(graph_name)
     graph = cfpq_data.graph_from_csv(graph_path)
     labels = cfpq_data.get_sorted_labels(graph)
@@ -15,9 +14,7 @@ def get_graph_stats(
     return nodes, edges, labels
 
 
-def generate_two_cycles_graph(
-        n1, n2, label1, label2,
-        file_name, graph_name="G"):
+def generate_two_cycles_graph(n1, n2, label1, label2, file_name, graph_name="G"):
     cfpq_graph = cfpq_data.labeled_two_cycles_graph(n1, n2, labels=(label1, label2))
     dot_graph = pydot.Dot(graph_name)
     edges = list(cfpq_graph.edges(data=True))
--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -4,19 +4,67 @@ from project.graph_utils import get_graph_stats
 
 def test_correct_name_1():
     result = get_graph_stats("skos")
-    expected_result = (144, 252, ['type', 'definition', 'isDefinedBy', 'label', 'subPropertyOf', 'comment', 'scopeNote',
-                                  'inverseOf', 'range', 'domain', 'contributor', 'disjointWith', 'creator', 'example',
-                                  'first', 'rest', 'description', 'seeAlso', 'subClassOf', 'title', 'unionOf'])
+    expected_result = (
+        144,
+        252,
+        [
+            "type",
+            "definition",
+            "isDefinedBy",
+            "label",
+            "subPropertyOf",
+            "comment",
+            "scopeNote",
+            "inverseOf",
+            "range",
+            "domain",
+            "contributor",
+            "disjointWith",
+            "creator",
+            "example",
+            "first",
+            "rest",
+            "description",
+            "seeAlso",
+            "subClassOf",
+            "title",
+            "unionOf",
+        ],
+    )
 
     assert result == expected_result
 
 
 def test_correct_name_2():
     result = get_graph_stats("travel")
-    expected_result = (131, 277, ['type', 'subClassOf', 'first', 'rest', 'disjointWith', 'onProperty', 'domain',
-                                  'range', 'someValuesFrom', 'comment', 'equivalentClass', 'intersectionOf',
-                                  'differentFrom', 'hasValue', 'hasPart', 'inverseOf', 'minCardinality', 'oneOf',
-                                  'complementOf', 'hasAccommodation', 'unionOf', 'versionInfo'])
+    expected_result = (
+        131,
+        277,
+        [
+            "type",
+            "subClassOf",
+            "first",
+            "rest",
+            "disjointWith",
+            "onProperty",
+            "domain",
+            "range",
+            "someValuesFrom",
+            "comment",
+            "equivalentClass",
+            "intersectionOf",
+            "differentFrom",
+            "hasValue",
+            "hasPart",
+            "inverseOf",
+            "minCardinality",
+            "oneOf",
+            "complementOf",
+            "hasAccommodation",
+            "unionOf",
+            "versionInfo",
+        ],
+    )
 
     assert result == expected_result
 
