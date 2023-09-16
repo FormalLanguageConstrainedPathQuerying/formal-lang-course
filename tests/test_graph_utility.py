d917020ad2e87c8abf07e629c99627da83856354
st084331 <71183199+st084331@users.noreply.github.com>
2023-09-16 20:02:17 +0300

    get_graph_info test added

--- tests/test_graph_utility.py
+++ tests/test_graph_utility.py
@@ -2,6 +2,63 @@ import filecmp
 import os
 from cfpq_data import labeled_two_cycles_graph
 from project.graph_utility.graph_utility import *
+from pytest import raises
+
+
+def test_load_graph():
+    # Graph from https://formallanguageconstrainedpathquerying.github.io/CFPQ_Data/reference/graphs/generated/cfpq_data.graphs.readwrite.csv.html#module-cfpq_data.graphs.readwrite.csv
+    graph = load_graph("generations")
+    assert graph.number_of_edges() == 273
+    assert graph.number_of_nodes() == 129
+    assert set(label for _, _, label in graph.edges.data(data="label")) == {
+        "hasSibling",
+        "someValuesFrom",
+        "versionInfo",
+        "sameAs",
+        "oneOf",
+        "range",
+        "first",
+        "type",
+        "hasValue",
+        "equivalentClass",
+        "intersectionOf",
+        "inverseOf",
+        "hasParent",
+        "onProperty",
+        "rest",
+        "hasChild",
+        "hasSex",
+    }
+
+
+def test_load_undefined_graph():
+    with raises(Exception):
+        load_graph("undefined")
+
+
+def test_get_graph_info():
+    graph_info = get_graph_info("generations")
+    assert graph_info[0] == 273
+    assert graph_info[1] == 129
+    assert graph_info[2] == {
+        "hasSibling",
+        "someValuesFrom",
+        "versionInfo",
+        "sameAs",
+        "oneOf",
+        "range",
+        "first",
+        "type",
+        "hasValue",
+        "equivalentClass",
+        "intersectionOf",
+        "inverseOf",
+        "hasParent",
+        "onProperty",
+        "rest",
+        "hasChild",
+        "hasSex",
+    }
 
 
 def test_create_graph_of_two_cycles():
