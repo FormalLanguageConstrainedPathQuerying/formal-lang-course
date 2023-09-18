041b4a30cf4517e04256b8eb8566e73239f6ea79
st084331 <71183199+st084331@users.noreply.github.com>
2023-09-19 01:07:42 +0300

    чтоб наверняка

--- project/automata_builder/DFA_builder.py
+++ project/automata_builder/DFA_builder.py
@@ -4,8 +4,5 @@ from pyformlang.finite_automaton import DeterministicFiniteAutomaton
 
 def build_mdfa(input: str) -> DeterministicFiniteAutomaton:
     enfa = Regex(input).to_epsilon_nfa()
-    if enfa.is_deterministic():
-        mdfa = enfa.to_deterministic().minimize()
-    else:
-        mdfa = enfa.minimize()
+    mdfa = enfa.to_deterministic().minimize()
     return mdfa
