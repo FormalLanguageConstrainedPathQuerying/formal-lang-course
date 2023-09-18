03bedc835d6046164c8413427f8d1f3b29d4f08a
st084331 <71183199+st084331@users.noreply.github.com>
2023-09-19 01:11:04 +0300

    some spaces

--- tests/automata_builder/DFA_builder_test.py
+++ tests/automata_builder/DFA_builder_test.py
@@ -5,10 +5,12 @@ from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
 def test_build_mdfa():
     input = "a.(b|c*)*"
     mdfa = build_mdfa(input)
+
     expected = NondeterministicFiniteAutomaton()
     expected.add_start_state(0)
     expected.add_final_state(1)
     expected.add_transitions([(0, "a", 1), (1, "b", 1), (1, "c", 1)])
+
     assert mdfa.is_deterministic()
     assert mdfa.minimize().is_equivalent_to(mdfa)
     assert mdfa.is_equivalent_to(expected)
