55f3a6fddb32c253963c9b5c7ef7a665870aa995
letit6E <rustam.letitbe@gmail.com>
2023-09-26 23:45:02 +0300

    Implement intersect and transitive closure methods for BoolMatrix

--- project/automata/bool_matrix.py
+++ project/automata/bool_matrix.py
@@ -10,7 +10,6 @@ class BoolMatrix:
             self.states, self.matrices = dict(), dict()
             return
 
-        self.num_of_states = len(nfa.states)
         self.start_states = nfa.start_states
         self.final_states = nfa.final_states
         self.states = {state: i for i, state in enumerate(nfa.states)}
@@ -29,3 +28,36 @@ class BoolMatrix:
                     self.matrices[symbol][
                         self.states[start_state], self.states[finish_state]
                     ] = True
+
+    def intersect(self, other: "BoolMatrix"):
+        result = BoolMatrix()
+
+        for symbol in self.matrices.keys() & other.matrices.keys():
+            result.matrices[symbol] = sparse.kron(
+                self.matrices[symbol], other.matrices[symbol], format='csr'
+            )
+
+        for self_state, self_index in self.states.items():
+            for other_state, other_index in other.states.items():
+                result_state = self_state * len(other.states) + other_index
+                result.states[result_state] = result_state
+
+                if self_state in self.start_states and other_state in other.start_states:
+                    result.start_states.add(result_state)
+
+                if self_state in self.final_states and other_state in other.final_states:
+                    result.final_states.add(result_state)
+
+        return result
+
+    def transitive_closure(self):
+        if len(self.matrices) == 0:
+            return sparse.csr_matrix((0, 0), dtype=bool)
+
+        result = sum(self.matrices.values())
+        prev_nnz = 0
+        while result.nnz != prev_nnz:
+            prev_nnz = result.nnz
+            result += result @ result
+
+        return result
