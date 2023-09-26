c0ccc4a37e02be6dbcf871c1394c686e14a2ae07
Anton Kraev <antonkraeww@gmail.com>
2023-09-26 23:20:04 +0300

    fixed variables name

--- project/rpq_tensors.py
+++ project/rpq_tensors.py
@@ -10,15 +10,15 @@ def regular_path_query(
     final_states: List[int],
     regex_query: str,
 ) -> Set[Tuple[int, int]]:
-    graph_automata = graph_to_nfa(graph, start_states, final_states)
-    regex_automata = regex_to_min_dfa(regex_query)
-    bool_graph = BoolDecompositionOfFA.from_fa(graph_automata)
-    bool_regex = BoolDecompositionOfFA.from_fa(regex_automata)
+    graph_automaton = graph_to_nfa(graph, start_states, final_states)
+    regex_automaton = regex_to_min_dfa(regex_query)
+    bool_graph = BoolDecompositionOfFA.from_fa(graph_automaton)
+    bool_regex = BoolDecompositionOfFA.from_fa(regex_automaton)
 
     intersection = BoolDecompositionOfFA.intersection(bool_graph, bool_regex)
     transitive_closure = BoolDecompositionOfFA.transitive_closure(intersection)
 
-    regex_automata_states_count = len(regex_automata.states)
+    regex_automata_states_count = len(regex_automaton.states)
     result = set()
     for start, final, label in transitive_closure.edges(data="label"):
         result.add(
