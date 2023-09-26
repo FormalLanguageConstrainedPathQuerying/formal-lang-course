c4ff38449b2e461f30ece1f0be081b6d18d09679
Danila Pechenev <d.pechenev@yadro.com>
2023-09-26 17:17:43 +0300

    Rewrite according to suggestions

--- project/rpq/automata_intersection_rpq.py
+++ project/rpq/automata_intersection_rpq.py
@@ -6,73 +6,85 @@ from pyformlang.finite_automaton import (
 )
 import networkx as nx
 from project.automaton_utils import *
+from project.graph_utils import get_graph_info
+from scipy.sparse import lil_matrix, kron
+import numpy as np
 
 
-def collect_labels_set(graph, start_state, end_state):
-    # It is assumed that `start_state` key exists in `graph` and `end_state` key exists in graph[start_state]
-    labels_set = set()
-    labels_dict = graph[start_state][end_state]
-    for key in labels_dict:
-        if "label" in labels_dict[key]:
-            labels_set.add(labels_dict[key]["label"])
-    return labels_set
+def boolean_decomposition(graph: nx.MultiDiGraph) -> dict[lil_matrix]:
+    graph_states = list(graph.adj.keys())
+    labels = get_graph_info(graph).labels_set
+    boolean_decomposition_matrices = {}
+    adjacency_matrix = nx.adjacency_matrix(graph).tocoo()
+    for label in labels:
+        boolean_decomposition_matrices[label] = lil_matrix(
+            np.zeros(adjacency_matrix.get_shape())
+        )
+    for start_vertex, end_vertex in zip(adjacency_matrix.row, adjacency_matrix.col):
+        labels_dict = graph[graph_states[start_vertex]][graph_states[end_vertex]]
+        for key in labels_dict:
+            if "label" in labels_dict[key]:
+                boolean_decomposition_matrices[labels_dict[key]["label"]][
+                    start_vertex, end_vertex
+                ] = 1
+    return boolean_decomposition_matrices
 
 
 def automata_intersection(
     automaton1: FiniteAutomaton, automaton2: FiniteAutomaton
 ) -> FiniteAutomaton:
-    states1, states2 = list(automaton1.states), list(automaton2.states)
+    start_states1 = list(automaton1.start_states)
+    start_states2 = list(automaton2.start_states)
+    for start_state in start_states1:
+        automaton1.remove_start_state(start_state)
+    for start_state in start_states2:
+        automaton2.remove_start_state(start_state)
     graph1, graph2 = automaton1.to_networkx(), automaton2.to_networkx()
     graph_states1 = list(graph1.adj.keys())
     graph_states2 = list(graph2.adj.keys())
-    ignore_indexes1 = [
-        i
-        for i in range(len(graph_states1))
-        if graph_states1[i] not in automaton1.states
-    ]
-    ignore_indexes2 = [
-        i
-        for i in range(len(graph_states2))
-        if graph_states2[i] not in automaton2.states
-    ]
-    adjacency_matrix1, adjacency_matrix2 = (
-        nx.adjacency_matrix(graph1).tocoo(),
-        nx.adjacency_matrix(graph2).tocoo(),
+    boolean_decomposition1 = boolean_decomposition(graph1)
+    boolean_decomposition2 = boolean_decomposition(graph2)
+    shared_labels = set(boolean_decomposition1.keys()) & set(
+        boolean_decomposition2.keys()
     )
-    states = [State((state1, state2)) for state2 in states2 for state1 in states1]
-    intersection = NondeterministicFiniteAutomaton(states)
-    for start1, end1 in zip(adjacency_matrix1.row, adjacency_matrix1.col):
-        if start1 in ignore_indexes1 or end1 in ignore_indexes1:
-            continue
-        for start2, end2 in zip(adjacency_matrix2.row, adjacency_matrix2.col):
-            if start2 in ignore_indexes2 or end2 in ignore_indexes2:
-                continue
-            start_state1, start_state2 = graph_states1[start1], graph_states2[start2]
-            end_state1, end_state2 = graph_states1[end1], graph_states2[end2]
-            for label in collect_labels_set(
-                graph1, start_state1, end_state1
-            ) & collect_labels_set(graph2, start_state2, end_state2):
-                intersection.add_transition(
-                    State((start_state1, start_state2)),
-                    Symbol(label),
-                    State((end_state1, end_state2)),
-                )
-    for i in automaton1.start_states:
-        for j in automaton2.start_states:
-            intersection.add_start_state(State((i, j)))
-    for i in automaton1.final_states:
-        for j in automaton2.final_states:
-            intersection.add_final_state(State((i, j)))
+    intersection = NondeterministicFiniteAutomaton(
+        [
+            State((state1, state2))
+            for state2 in automaton2.states
+            for state1 in automaton1.states
+        ]
+    )
+    for label in shared_labels:
+        kronecker_product = kron(
+            boolean_decomposition1[label], boolean_decomposition2[label]
+        ).tocoo()
+        for start_vertex, end_vertex in zip(
+            kronecker_product.row, kronecker_product.col
+        ):
+            start_state1 = graph_states1[
+                start_vertex // boolean_decomposition2[label].get_shape()[0]
+            ]
+            start_state2 = graph_states2[
+                start_vertex % boolean_decomposition2[label].get_shape()[0]
+            ]
+            end_state1 = graph_states1[
+                end_vertex // boolean_decomposition2[label].get_shape()[1]
+            ]
+            end_state2 = graph_states2[
+                end_vertex % boolean_decomposition2[label].get_shape()[1]
+            ]
+            intersection.add_transition(
+                State((start_state1, start_state2)),
+                Symbol(label),
+                State((end_state1, end_state2)),
+            )
+    for start_state1 in start_states1:
+        for start_state2 in start_states2:
+            intersection.add_start_state(State((start_state1, start_state2)))
+    for final_state1 in automaton1.final_states:
+        for final_state2 in automaton2.final_states:
+            intersection.add_final_state(State((final_state1, final_state2)))
     return intersection
-
-
-def new_path_exists(graph, start_state, end_state):
-    # It is assumed that `start_state` key exists in `graph` and `end_state` key exists in graph[start_state]
-    labels_dict = graph[start_state][end_state]
-    for key in labels_dict:
-        if len(labels_dict[key]) == 0:
-            return True
-    return False
 
 
 def automata_intersection_rpq(
@@ -86,20 +98,22 @@ def automata_intersection_rpq(
     regex_automaton = regex_to_dfa(regex)
     adjacency_matrix = nx.adjacency_matrix(graph)
     previous_count_nonzero = None
-    nonterminal = Symbol(None)
+    nonterminal = Symbol("NONTERMINAL")
     while adjacency_matrix.count_nonzero() != previous_count_nonzero:
         previous_count_nonzero = adjacency_matrix.count_nonzero()
-        intersection = automata_intersection(graph_automaton, regex_automaton)
-        transitive_closure = nx.transitive_closure(intersection.to_networkx(), None)
+        intersection = automata_intersection(
+            graph_automaton.copy(), regex_automaton.copy()
+        )
+        intersection_graph = intersection.to_networkx()
+        intersection_adjacency_matrix = nx.adjacency_matrix(intersection_graph).tolil()
+        transitive_closure = nx.transitive_closure(intersection_graph, None)
         transitive_closure_states = list(transitive_closure.adj.keys())
-        intersection_adjacency_matrix = nx.adjacency_matrix(transitive_closure).tocoo()
-        for i, j in zip(
-            intersection_adjacency_matrix.row, intersection_adjacency_matrix.col
-        ):
+        transitive_closure_matrix = nx.adjacency_matrix(transitive_closure).tocoo()
+        for i, j in zip(transitive_closure_matrix.row, transitive_closure_matrix.col):
             state1 = transitive_closure_states[i]
             state2 = transitive_closure_states[j]
             if (
-                new_path_exists(transitive_closure, state1, state2)
+                intersection_adjacency_matrix[i, j] == 0
                 and state1[1] in regex_automaton.start_states
                 and state2[1] in regex_automaton.final_states
             ):
@@ -107,6 +121,7 @@ def automata_intersection_rpq(
                     State(state1[0]), nonterminal, State(state2[0])
                 )
         adjacency_matrix = nx.adjacency_matrix(graph_automaton.to_networkx())
+
     adjacency_matrix = adjacency_matrix.tocoo()
     graph_states = list(graph.adj.keys())
     ignore_indexes = [
--- tests/test_automata_intersection_rpq.py
+++ tests/test_automata_intersection_rpq.py
@@ -1,34 +1,9 @@
 from project.automaton_utils import *
 from project.rpq.automata_intersection_rpq import *
 from pyformlang.finite_automaton import Symbol, State, NondeterministicFiniteAutomaton
-import networkx as nx
 
 
-def test_collect_labels_set():
-    automaton = NondeterministicFiniteAutomaton()
-    automaton.add_transition(State(0), Symbol("a"), State(1))
-    automaton.add_transition(State(1), Symbol("b"), State(2))
-    automaton.add_transition(State(2), Symbol("c"), State(3))
-    automaton.add_transition(State(0), Symbol("u"), State(1))
-    automaton.add_transition(State(0), Symbol("v"), State(1))
-    automaton.add_transition(State(3), Symbol("t"), State(0))
-    graph = automaton.to_networkx()
-    assert collect_labels_set(graph, 0, 1) == {"a", "u", "v"}
-
-
-def test_collect_labels_set_empty():
-    automaton = NondeterministicFiniteAutomaton()
-    automaton.add_transition(State(0), Symbol("a"), State(1))
-    automaton.add_transition(State(1), Symbol("b"), State(2))
-    automaton.add_transition(State(2), Symbol("c"), State(3))
-    automaton.add_transition(State(0), Symbol("u"), State(1))
-    automaton.add_transition(State(0), Symbol("v"), State(1))
-    automaton.add_transition(State(3), Symbol("t"), State(0))
-    graph = nx.transitive_closure(automaton.to_networkx(), None)
-    assert len(collect_labels_set(graph, 0, 2)) == 0
-
-
-def test_automata_intersection():
+def test_automata_intersection1():
     automaton1 = regex_to_dfa("(a|b).(c|a).(a*)")
     automaton2 = regex_to_dfa("a*")
     intersection = automata_intersection(automaton1, automaton2)
@@ -40,7 +15,29 @@ def test_automata_intersection():
     assert not intersection.accepts([Symbol("b"), Symbol("c")])
 
 
-def test_reachability_problem1():
+def test_automata_intersection2():
+    automaton1 = regex_to_dfa("(ab|ac).(d*)")
+    automaton2 = regex_to_dfa("((ab)*).(d)")
+    intersection = automata_intersection(automaton1, automaton2)
+    assert not intersection.accepts([Symbol("ab")])
+    assert not intersection.accepts([Symbol("ac")])
+    assert intersection.accepts([Symbol("ab"), Symbol("d")])
+    assert not intersection.accepts([Symbol("ab"), Symbol("d"), Symbol("d")])
+
+
+def test_automata_intersection3():
+    automaton1 = regex_to_dfa("(a*).(b*).(c*)")
+    automaton2 = regex_to_dfa("b.(c*)")
+    intersection = automata_intersection(automaton1, automaton2)
+    assert not intersection.accepts([Symbol("a")])
+    assert intersection.accepts([Symbol("b")])
+    assert not intersection.accepts([Symbol("c")])
+    assert intersection.accepts([Symbol("b"), Symbol("c")])
+    assert not intersection.accepts([Symbol("b"), Symbol("b"), Symbol("c")])
+    assert intersection.accepts([Symbol("b"), Symbol("c"), Symbol("c")])
+
+
+def test_rpq1():
     automaton = NondeterministicFiniteAutomaton()
     automaton.add_transition(State(0), Symbol("a"), State(1))
     automaton.add_transition(State(1), Symbol("b"), State(2))
@@ -50,7 +47,7 @@ def test_reachability_problem1():
     assert automata_intersection_rpq(graph, "(a|f).(b|d)", {0}, {2, 4}) == {(0, 2)}
 
 
-def test_reachability_problem2():
+def test_rpq2():
     automaton = NondeterministicFiniteAutomaton()
     automaton.add_transition(State(0), Symbol("d"), State(1))
     automaton.add_transition(State(1), Symbol("c"), State(2))
@@ -67,7 +64,7 @@ def test_reachability_problem2():
     }
 
 
-def test_reachability_problem3():
+def test_rpq3():
     automaton = NondeterministicFiniteAutomaton()
     automaton.add_transition(State(0), Symbol("a"), State(1))
     automaton.add_transition(State(1), Symbol("a"), State(2))
