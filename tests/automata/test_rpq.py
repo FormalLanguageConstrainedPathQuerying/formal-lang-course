5348642531d1b17f19adbe3806b9a01e9f39106b
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-09-27 23:04:27 +0300

    Implement tests for rpq

---
+++ tests/automata/test_rpq.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import networkx
+from pyformlang.regular_expression import Regex
+
+from project.automata.builders import build_nfa
+from project.automata.rpq import rpq
+
+
+class TestsForRpq:
+    def test_rpq_not_empty(self):
+        graph = networkx.nx_pydot.read_dot(Path("./resources/dfa4.dot"))
+        # dfa that accepts regex "a.b|a.c|(c.d)*"
+        regex = Regex("a.b|a.c")
+
+        expected = {("2", "1")}
+
+        assert expected == rpq(
+            regex, graph, start_nodes={"2"}, final_nodes={"0", "1", "2"}
+        )
+
+    def test_rpq_empty(self):
+        graph = networkx.nx_pydot.read_dot(Path("./resources/dfa4.dot"))
+        # dfa that accepts regex "a.b|a.c|(c.d)*"
+        regex = Regex("$")
+
+        expected = {("2", "2")}
+
+        assert expected == rpq(
+            regex, graph, start_nodes={"2"}, final_nodes={"0", "1", "2"}
+        )
