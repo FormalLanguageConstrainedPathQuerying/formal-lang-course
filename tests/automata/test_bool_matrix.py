681628e07612530ffe0a61618ef07595cc687810
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-09-27 22:06:31 +0300

    Add tests for BoolMatrix.transitive_closure

--- tests/automata/test_bool_matrix.py
+++ tests/automata/test_bool_matrix.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
+import networkx
+from numpy import ndarray, array
 from pyformlang.regular_expression import Regex
 
 from project.automata.bool_matrix import BoolMatrix
-from project.automata.builders import build_minimal_dfa
+from project.automata.builders import build_minimal_dfa, build_nfa
 
 
 class TestsForBoolMatrix:
@@ -37,3 +41,24 @@ class TestsForBoolMatrix:
         nfa = bm_intersected.to_nfa()
 
         assert nfa.is_equivalent_to(expected_automata)
+
+    def test_transitive_closure(self):
+        graph = networkx.nx_pydot.read_dot(Path("./resources/dfa3.dot"))
+
+        nfa = build_nfa(graph, start_states={"0"}, final_states={"2"})
+        bm = BoolMatrix(nfa)
+        trc = bm.transitive_closure().toarray()
+
+        expected = {
+            "0": {"0": False, "1": True, "2": True, "3": True, "4": True},
+            "1": {"0": False, "1": False, "2": True, "3": True, "4": True},
+            "2": {"0": False, "1": False, "2": False, "3": True, "4": True},
+            "3": {"0": False, "1": False, "2": False, "3": True, "4": False},
+            "4": {"0": False, "1": False, "2": False, "3": False, "4": False},
+        }
+
+        assert all(
+            trc[bm.states[start_state]][bm.states[final_state]] == result
+            for start_state, row in expected.items()
+            for final_state, result in row.items()
+        )
