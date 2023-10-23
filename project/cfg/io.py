35678d0972a45b2db5407bb0714f83ee3ade453c
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-10-23 21:10:07 +0300

    Cosmetics changes and add docs to cfg packages functions

--- project/cfg/io.py
+++ project/cfg/io.py
@@ -3,9 +3,36 @@ from pathlib import Path
 from pyformlang.cfg import CFG, Variable
 
 
-def read_from_file(path: Path, start_non_terminal: str = None) -> CFG:
-    if start_non_terminal is None:
-        start_non_terminal = "S"
+def read_from_file(path: Path, start_symbol: str = None) -> CFG:
+    """Read a context free grammar from a file.
+    The file contains one rule per line.
+    The structure of a production is:
+    head -> body1 | body2 | ... | bodyn
+    where | separates the bodies.
+    A variable (or non terminal) begins by a capital letter.
+    A terminal begins by a non-capital character
+    Terminals and Variables are separated by spaces.
+    An epsilon symbol can be represented by epsilon, $, ε, ϵ or Є.
+    If you want to have a variable name starting with a non-capital \
+    letter or a terminal starting with a capital letter, you can \
+    explicitly give the type of your symbol with "VAR:yourVariableName" \
+    or "TER:yourTerminalName" (with the quotation marks). For example:
+    S -> "TER:John" "VAR:d" a b
+
+    Parameters
+    ----------
+    path : Path
+        The path of inputted file
+    start_symbol : str, optional
+        The start symbol, S by default
+
+    Returns
+    -------
+    cfg : CFG
+        A context free grammar.
+    """
+    if start_symbol is None:
+        start_symbol = "S"
     with open(path, "r") as file:
         text_cfg = file.read()
-    return CFG.from_text(text_cfg, Variable(start_non_terminal))
+    return CFG.from_text(text_cfg, Variable(start_symbol))
--- project/cfg/transformers.py
+++ project/cfg/transformers.py
@@ -2,6 +2,15 @@ from pyformlang.cfg import CFG
 
 
 def transform_to_wcnf(cfg: CFG):
+    """Transforms a CFG to the Weak Chomsky Normal Form
+    if possible without transform to Chomsky Normal Form
+
+    Returns
+    ----------
+    new_cfg : CFG
+        A new CFG equivalent in the WCNF form
+
+    """
     new_cfg = cfg.eliminate_unit_productions().remove_useless_symbols()
     new_productions = new_cfg._decompose_productions(
         new_cfg._get_productions_with_only_single_terminals()
