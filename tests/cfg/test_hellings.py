27ad050bf5b076a4aed34eac4113e55e9d960d5c
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-13 20:52:12 +0300

    Reformat code in tests

--- tests/cfg/test_hellings.py
+++ tests/cfg/test_hellings.py
@@ -20,9 +20,21 @@ class TestForHellingCfpq:
 
         # Test from lecture notes (page 113)
         assert cfpq(cfg, graph) == {
-            (0, Variable("S"), 2), (2, Variable("C"), 3), (1, Variable("A"), 2), (1, Variable("S"), 2),
-            (2, Variable("S"), 2), (0, Variable("S"), 3), (0, Variable("A"), 1), (0, Variable("C"), 2),
-            (1, Variable("S"), 3), (2, Variable("S"), 3), (1, Variable("C"), 2), (3, Variable("B"), 2),
-            (2, Variable("A"), 0), (2, Variable("C"), 2), (2, Variable("B"), 3), (0, Variable("C"), 3),
-            (1, Variable("C"), 3)
+            (0, Variable("S"), 2),
+            (2, Variable("C"), 3),
+            (1, Variable("A"), 2),
+            (1, Variable("S"), 2),
+            (2, Variable("S"), 2),
+            (0, Variable("S"), 3),
+            (0, Variable("A"), 1),
+            (0, Variable("C"), 2),
+            (1, Variable("S"), 3),
+            (2, Variable("S"), 3),
+            (1, Variable("C"), 2),
+            (3, Variable("B"), 2),
+            (2, Variable("A"), 0),
+            (2, Variable("C"), 2),
+            (2, Variable("B"), 3),
+            (0, Variable("C"), 3),
+            (1, Variable("C"), 3),
         }
