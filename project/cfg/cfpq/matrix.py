c79ff662a358c8b082eed3bc2d366de97ac39f84
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-23 23:55:09 +0300

    Implement matrix cfpq algorithm

---
+++ project/cfg/cfpq/matrix.py
@@ -0,0 +1,16 @@
+from networkx import MultiDiGraph
+from pyformlang.cfg import CFG, Variable
+
+
+def cfpq_all(cfg: CFG, graph: MultiDiGraph):
+    return None
+
+
+def cfpq(
+    cfg: CFG,
+    graph: MultiDiGraph,
+    start_nodes: set = None,
+    final_nodes: set = None,
+    start_symbol: Variable = Variable("S"),
+):
+    return None
