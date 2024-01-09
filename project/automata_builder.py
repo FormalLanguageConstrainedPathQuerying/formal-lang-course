ecc7d88648882d05fa8c64eacbf0614d5103618b
Naniduan <41004075+Naniduan@users.noreply.github.com>
2024-01-09 20:45:54 +0300

    Task 2

---
+++ project/automata_builder.py
@@ -0,0 +1,56 @@
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+import cfpq_data
+import pydot
+from pyformlang.finite_automaton import EpsilonNFA, State, Symbol, Epsilon
+
+__all__ = ["get_dfa_from_regex", "get_nfa_from_graph"]
+
+
+def get_dfa_from_regex(regex):
+    """takes a regular expression and returns an equivalent deterministic final automaton"""
+
+    enfa = regex.to_epsilon_nfa()
+    dfa = enfa.to_deterministic()
+    return dfa
+
+
+def get_nfa_from_graph(graph, start=[], final=[]):
+    """takes a graph and generates a non-deterministic final automaton based on it
+    lists of start and final states/nodes can be given, otherwise all states
+    will be start and final"""
+
+    edges = list(graph.edges(data="label", default="epsilon"))
+    nodes = list(graph.nodes())
+    nfa = EpsilonNFA()
+
+    states = dict()
+    for node in nodes:
+        states[node] = State(node)
+
+    if not start:
+        for node in nodes:
+            nfa.add_start_state(states[node])
+    else:
+        for node in start:
+            nfa.add_start_state(states[node])
+
+    if not final:
+        for node in nodes:
+            nfa.add_final_state(states[node])
+    else:
+        for node in final:
+            nfa.add_final_state(states[node])
+
+    symbols = dict()
+    for edge in edges:
+
+        if not edge[2] in symbols:
+            if edge[2] == "epsilon":
+                symbols[edge[2]] = Epsilon()
+            else:
+                symbols[edge[2]] = Symbol(edge[2])
+
+        nfa.add_transition(states[edge[0]], symbols[edge[2]], states[edge[1]])
+
+    return nfa
---
+++ tests/test_automata_builder.py
@@ -0,0 +1,79 @@
+import pytest
+from project.automata_builder import *
+import networkx as nx
+
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+
+
+def test_equivalence_1():
+    regex = Regex("(a|b|c) (d|e|f)")
+    dfa = get_dfa_from_regex(regex)
+
+    assert regex.accepts(["a", "d"]) == dfa.accepts(["a", "d"])
+
+
+def test_equivalence_2():
+    regex = Regex("abc|d")
+    dfa = get_dfa_from_regex(regex)
+
+    assert regex.accepts(["d"]) == dfa.accepts(["d"])
+
+
+def test_equivalence_3():
+    regex = Regex("abc|d")
+    dfa = get_dfa_from_regex(regex)
+
+    assert regex.accepts(["a", "b", "c"]) == dfa.accepts(["a", "b", "c"])
+
+
+def test_graph_the_amount_of_a_mod_3_is_0_correct_1():
+    graph = nx.MultiDiGraph()
+    graph.add_nodes_from([0, 1, 2])
+
+    graph.add_edges_from([(0, 1, dict(label="a")), (0, 0, dict(label="b"))])
+    graph.add_edges_from([(1, 2, dict(label="a")), (1, 1, dict(label="b"))])
+    graph.add_edges_from([(2, 0, dict(label="a")), (2, 2, dict(label="b"))])
+
+    nfa = get_nfa_from_graph(graph, start=[0], final=[0])
+
+    assert nfa.accepts("aaa") == True
+
+
+def test_graph_the_amount_of_a_mod_3_is_0_correct_2():
+    graph = nx.MultiDiGraph()
+    graph.add_nodes_from([0, 1, 2])
+
+    graph.add_edges_from([(0, 1, dict(label="a")), (0, 0, dict(label="b"))])
+    graph.add_edges_from([(1, 2, dict(label="a")), (1, 1, dict(label="b"))])
+    graph.add_edges_from([(2, 0, dict(label="a")), (2, 2, dict(label="b"))])
+
+    nfa = get_nfa_from_graph(graph, start=[0], final=[0])
+
+    assert nfa.accepts("abbbaabababbaababa") == True
+
+
+def test_graph_the_amount_of_a_mod_3_is_0_incorrect_1():
+    graph = nx.MultiDiGraph()
+    graph.add_nodes_from([0, 1, 2])
+
+    graph.add_edges_from([(0, 1, dict(label="a")), (0, 0, dict(label="b"))])
+    graph.add_edges_from([(1, 2, dict(label="a")), (1, 1, dict(label="b"))])
+    graph.add_edges_from([(2, 0, dict(label="a")), (2, 2, dict(label="b"))])
+
+    nfa = get_nfa_from_graph(graph, start=[0], final=[0])
+
+    assert nfa.accepts("aa") == False
+
+
+def test_graph_the_amount_of_a_mod_3_is_0_incorrect_2():
+    graph = nx.MultiDiGraph()
+    graph.add_nodes_from([0, 1, 2])
+
+    graph.add_edges_from([(0, 1, dict(label="a")), (0, 0, dict(label="b"))])
+    graph.add_edges_from([(1, 2, dict(label="a")), (1, 1, dict(label="b"))])
+    graph.add_edges_from([(2, 0, dict(label="a")), (2, 2, dict(label="b"))])
+
+    nfa = get_nfa_from_graph(graph, start=[0], final=[0])
+
+    assert nfa.accepts("abbbaababaabbaababa") == False
