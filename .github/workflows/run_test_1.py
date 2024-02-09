5a9421ee2d0703dfbdaf8d0744ab672a042e1f8b
MAC <90581598+sideov@users.noreply.github.com>
2024-02-10 02:50:28 +0300

    done

--- .github/workflows/run_test_1.py
+++ .github/workflows/run_test_1.py
@@ -1,14 +1,27 @@
 import pytest
 import os
+import pathlib
+
+ROOT = pathlib.Path(__file__).parent.parent.parent
+TESTS = ROOT / "tests"
+
+def configure_python_path():
+    python_path = os.getenv("PYTHONPATH")
+
+    if python_path is None:
+        os.environ["PYTHONPATH"] = str(ROOT)
+    else:
+        os.environ["PYTHONPATH"] += ";" + str(ROOT)
 
 def run_tests():
-    cur_path = os.getcwd()
-    os.chdir("../../tests/")
+    os.chdir(TESTS)
 
     pytest.main(["test_1.py"])
 
-    os.chdir(cur_path)
+    os.chdir(ROOT)
+
 
 if __name__ == "__main__":
+    configure_python_path()
     run_tests()
 
