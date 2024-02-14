3838434a22142ef2d33fb7fc37462d76a597ca67
z1kurat <109900803+z1kurat@users.noreply.github.com>
2024-02-14 23:13:54 +0300

    Done Task 1

--- project/__init__.py
+++ project/__init__.py
@@ -1,1 +1,3 @@
-print("import sources directory")
+from .graph import get_graph_info_by_name, create_labeled_two_cycles_graph
+
+__all__ = ['get_graph_info_by_name', 'create_labeled_two_cycles_graph']
