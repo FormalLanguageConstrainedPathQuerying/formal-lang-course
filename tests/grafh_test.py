cd61241fc86faca0fde86de6978c5d084f6b8ebb
unknown <sveta-bakshina@mail.ru>
2024-02-14 22:00:08 +0300

    commit

--- tests/grafh_test.py
+++ tests/grafh_test.py
@@ -1,13 +1,13 @@
 import pytest
 import os
-from Grafhs import build_and_save_graph, get_graph_info
-'''
+from project.task_1 import get_graph_info, build_and_save_graph
+
 def test_get_graph_info():
     num_vertices, num_edges, edge_labels = get_graph_info("example_graph")
     assert num_vertices == 3
     assert num_edges == 3
     assert set(edge_labels) == {'A', 'B', 'C'}
-'''
+
 def test_build_and_save_graph(tmp_path):
     output_file = os.path.join(tmp_path, "output_graph.dot")
     build_and_save_graph(3, ['A', 'B', 'C'], output_file)
