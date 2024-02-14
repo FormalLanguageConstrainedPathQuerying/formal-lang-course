099660c73d7d38249b7179cf7f7109888c4934f8
z1kurat <109900803+z1kurat@users.noreply.github.com>
2024-02-14 23:26:35 +0300

    style fix

--- project/graph.py
+++ project/graph.py
@@ -12,19 +12,16 @@ def get_graph_info_by_name(name: str) -> Tuple[int, int, Set[str]]:
     graph_path: pathlib.Path = cfpq_data.download(name)
     graph: nx.MultiDiGraph = cfpq_data.graph_from_csv(graph_path)
 
-    return graph.number_of_nodes(), graph.number_of_edges(), get_graph_label(graph=graph)
+    return (
+        graph.number_of_nodes(),
+        graph.number_of_edges(),
+        get_graph_label(graph=graph)
+    )
 
 def create_labeled_two_cycles_graph(
-    n: int,
-    m: int,
-    labels: Tuple[str, str],
-    path: str,
-    **kwargs
+    n: int, m: int, labels: Tuple[str, str], path: str, **kwargs
 ) -> None:
     graph: nx.MultiDiGraph = cfpq_data.labeled_two_cycles_graph(
-        n=n,
-        m=m,
-        labels=labels,
-        **kwargs
+        n=n, m=m, labels=labels, **kwargs
     )
     nx.drawing.nx_pydot.write_dot(G=graph, path=path)
--- tests/test_task_1.py
+++ tests/test_task_1.py
@@ -5,12 +5,15 @@ from typing import Set
 
 import pytest
 
+
 @pytest.fixture()
 def start():
     print("Start test")
 
+
 def test_get_graph_info_by_name_bzip():
-    assert get_graph_info_by_name("bzip") == (632, 556, {'d', 'a'})
+    assert get_graph_info_by_name("bzip") == (632, 556, {"d", "a"})
+
 
 def test_get_graph_info_by_name_generations():
     labels: Set = {
@@ -35,27 +38,23 @@ def test_get_graph_info_by_name_generations():
 
     assert get_graph_info_by_name("generations") == (129, 273, labels)
 
+
 def test_create_labeled_two_cycles_graph(path: str ='graph.dot'):
-    create_labeled_two_cycles_graph(
-        n=5,
-        m=4,
-        labels=("a", "b"),
-        path=path
-    )
+    create_labeled_two_cycles_graph(n=5, m=4, labels=("a", "b"), path=path)
 
     graph = nx.Graph(nx.nx_pydot.read_dot(path))
 
-    assert list(graph.nodes) == ['1', '2', '3', '4', '5', '0', '6', '7', '8', '9']
+    assert list(graph.nodes) == ["1", "2", "3", "4", "5", "0", "6", "7", "8", "9"]
     assert list(graph.edges) == [
-        ('1', '2'),
-        ('1', '0'),
-        ('2', '3'),
-        ('3', '4'),
-        ('4', '5'),
-        ('5', '0'),
-        ('0', '6'),
-        ('0', '9'),
-        ('6', '7'),
-        ('7', '8'),
-        ('8', '9')
+        ("1", "2"),
+        ("1", "0"),
+        ("2", "3"),
+        ("3", "4"),
+        ("4", "5"),
+        ("5", "0"),
+        ("0", "6"),
+        ("0", "9"),
+        ("6", "7"),
+        ("7", "8"),
+        ("8", "9"),
     ]
