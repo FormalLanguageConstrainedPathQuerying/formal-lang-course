17b67179882ec40d9db3c4a8c73caf27c63fc947
MAC <90581598+sideov@users.noreply.github.com>
2024-02-21 04:47:16 +0300

    done

--- project/task2.py
+++ project/task2.py
@@ -5,6 +5,7 @@ from pyformlang.finite_automaton import State, Epsilon
 from pyformlang.finite_automaton import Symbol
 import networkx as nx
 from typing import Set
+import copy
 
 
 def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton or None:
@@ -24,26 +25,41 @@ def graph_to_nfa(
 ) -> NondeterministicFiniteAutomaton:
     nfa = NondeterministicFiniteAutomaton()
 
-    if start_nodes is None:
+    if len(start_nodes) == 0 or start_nodes is None:
         start_nodes = graph.nodes
     else:
         start_nodes = []
 
-    if final_nodes is None:
+    if len(final_nodes) == 0 or final_nodes is None:
         final_nodes = graph.nodes
     else:
         final_nodes = []
 
+    for node in graph.nodes:
+        graph.nodes(data=True)[node]["is_start"] = False
+        graph.nodes(data=True)[node]["is_final"] = False
+
     for node in start_nodes:
+        graph.nodes(data=True)[node]["is_start"] = True
         nfa.add_start_state(State(node))
 
     for node in final_nodes:
+        graph.nodes(data=True)[node]["is_final"] = True
         nfa.add_final_state(State(node))
+
+    eps_edges = []
 
     for edge in graph.edges(data=True):
         if edge[2]["label"] == "eps":
-            nfa.add_transition(State(edge[0]), Epsilon(), State(edge[1]))
+            eps_edges.append(edge)
         else:
             nfa.add_transition(State(edge[0]), Symbol(edge[2]["label"]), State(edge[1]))
+
+    for e in eps_edges:
+        e_to = e[1]
+        e_from = e[0]
+        for e2 in graph.edges(data=True):
+            if e2[0] == e_to:
+                nfa.add_transition(State(e_from), Symbol(e2[2]["label"]), State(e2[1]))
 
     return nfa
