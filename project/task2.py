19ccbbabdd54878210ebde1c725b251588231645
sivalien <sivalien@icloud.com>
2024-02-28 01:49:21 +0300

    add hw 2

--- project/task2.py
+++ project/task2.py
@@ -8,37 +8,23 @@ from typing import Set
 
 
 def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
-    regex_obj = Regex(regex)
-    nfa = regex_obj.to_epsilon_nfa()
-    dfa = nfa.to_deterministic()
-    return dfa.minimize()
+    return Regex(regex).to_epsilon_nfa().to_deterministic().minimize()
 
 
 def graph_to_nfa(
-        graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
+    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
 ) -> NondeterministicFiniteAutomaton:
     nfa = NondeterministicFiniteAutomaton()
 
-    if start_states is None:
-        start_states = set(graph.nodes)
-    if final_states is None:
-        final_states = set(graph.nodes)
+    all_nodes = set(graph.nodes())
 
-    for state in start_states:
+    for state in start_states or all_nodes:
         nfa.add_start_state(State(state))
 
-    for state in final_states:
+    for state in final_states or all_nodes:
         nfa.add_final_state(State(state))
 
-    # Добавляем состояния из вершин графа
-    # for node in graph.nodes:
-    #     nfa.add_state(State(node))
-
-    # Добавляем переходы между состояниями
-    for begin, end, data in graph.edges(data=True):
-        from_state = State(begin)
-        to_state = State(end)
-        symbol = Symbol(data["label"])
-        nfa.add_transition(from_state, symbol, to_state)
+    for begin, end, label in graph.edges.data("label"):
+        nfa.add_transition(State(begin), Symbol(label), State(end))
 
     return nfa
