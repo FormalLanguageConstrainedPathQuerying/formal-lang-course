7b2a047b4803ec189fc5ad94e035e3d2384f1930
unknown <sveta-bakshina@mail.ru>
2024-02-28 19:29:23 +0300

    task-2

--- project/task2.py
+++ project/task2.py
@@ -1,43 +1,39 @@
-from pyformlang.finite_automaton import DeterministicFiniteAutomaton, NondeterministicFiniteAutomaton
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
 from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
-from typing import Iterable, Any, List
 from pyformlang.regular_expression import Regex
-import networkx as nx
+from networkx import MultiDiGraph
+from typing import Set
 
-
-def regex_to_dfa(expr: str) -> DeterministicFiniteAutomaton:
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
     """Return minimized DFA from regular expression string
 
     Keyword arguments:
     expr -- academic regular expression string;
     """
-    dfa = Regex(expr).to_epsilon_nfa()
+    dfa = Regex(regex).to_epsilon_nfa()
     return dfa.minimize()
 
 
 def graph_to_nfa(
-        graph: nx.MultiDiGraph,
-        starts: Iterable[Any] = None,
-        finals: Iterable[Any] = None,
-) -> list[Any] | NondeterministicFiniteAutomaton:
-    """Return nondeterministic finite automaton from :class:`nx.MultiDiGraph` graph
+    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
+) -> NondeterministicFiniteAutomaton:
+    """Return nondeterministic finite automaton from `networkx.MultiDiGraph` graph
 
     Keyword arguments:
     graph -- source graph;
-    starts -- `graph's` nodes marked as starts;
-    finals -- `graph's` nodes marked as finals;
+    start_states -- set of nodes marked as start states;
+    final_states -- set of nodes marked as final states;
     """
-    if not graph.edges() or (starts is None and finals is None):
-        return []
     nfa = NondeterministicFiniteAutomaton()
 
-    if starts is not None:
-        for node in starts:
-            nfa.add_start_state(node)
-    if finals is not None:
-        for node in finals:
-            nfa.add_final_state(node)
+    for node in start_states:
+        nfa.add_start_state(node)
 
-    nfa.add_transitions([(v, d["label"], u) for v, u, d in graph.edges(data=True)])
+    for node in final_states:
+        nfa.add_final_state(node)
+
+    for source, dest, label in graph.edges(data="label", default=None):
+        if label is not None:
+            nfa.add_transition(source, label, dest)
 
     return nfa
