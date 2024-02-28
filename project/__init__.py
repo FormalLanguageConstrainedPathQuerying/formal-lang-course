c7d5b3e44c38076fb2b26d111e8409cc8cd4a5b4
MAC <90581598+sideov@users.noreply.github.com>
2024-02-28 16:01:08 +0300

    all is done

--- project/__init__.py
+++ project/__init__.py
@@ -1,1 +1,4 @@
-print("import sources directory")
+# from project.task3 import intersect_automata, FiniteAutomaton
+# from project.task2 import regex_to_dfa
+#
+# __all__ = [regex_to_dfa, intersect_automata, FiniteAutomaton]
