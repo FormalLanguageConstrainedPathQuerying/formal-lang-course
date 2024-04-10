6c611d4c5e9702d26196a78d325c3734b59131fc
MAC <90581598+sideov@users.noreply.github.com>
2024-04-10 22:21:34 +0300

    d

--- tests/test_6.py
+++ tests/test_6.py
@@ -4,11 +4,12 @@ from pyformlang.cfg import CFG, Variable
 from pyformlang.cfg import Production, Variable, Terminal, CFG, Epsilon
 import networkx as nx
 from pyformlang.cfg import CFG
+import os
 
 
 def test_grammar_weak1():
 
-    g = read_cfgrammar("../tests/filesForTest/cfgTest1")
+    g = read_cfgrammar("tests/filesForTest/cfgTest1")
 
     actual = cfg_to_weak_normal_form(g)
 
@@ -30,7 +31,7 @@ def test_grammar_weak1():
 
 def test_grammar_weak2():
 
-    actual = cfg_to_weak_normal_form(read_cfgrammar("../tests/filesForTest/cfgTest2"))
+    actual = cfg_to_weak_normal_form(read_cfgrammar("tests/filesForTest/cfgTest2"))
     expected = CFG.from_text(
         """
     S -> NP VP
