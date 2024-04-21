dade111d1d8cc15d8c933fd59cab8486452c90a0
Sergo Kovaltsov <sergeykovaltsov@gmail.com>
2024-04-22 02:37:11 +0300

    Done

---
+++ project/task07/__init__.py
@@ -0,0 +1,3 @@
+from project.task07.main import cfpq_with_matrix
+
+__all__ = [cfpq_with_matrix]
--- tests/autotests/test_task7.py
+++ tests/autotests/test_task7.py
@@ -13,11 +13,11 @@ from pyformlang import cfg
 
 # Fix import statements in try block to run tests
 try:
-    from project.task2 import graph_to_nfa, regex_to_dfa
-    from project.task3 import FiniteAutomaton
-    from project.task4 import reachability_with_constraints
-    from project.task7 import cfpq_with_matrix
-    from project.task6 import cfpq_with_hellings
+    from project.task02 import graph_to_nfa, regex_to_dfa
+    from project.task03 import FiniteAutomaton
+    from project.task04 import reachability_with_constraints
+    from project.task06 import cfpq_with_hellings
+    from project.task07 import cfpq_with_matrix
 except ImportError:
     pytestmark = pytest.mark.skip("Task 7 is not ready to test!")
 
