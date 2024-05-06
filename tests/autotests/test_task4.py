336832baa375832796238a6f4072497c22a8a9d0
Efim Kubishkin <efimkub@mail.ru>
2024-05-07 00:06:12 +0300

    Rework test for task 4

--- tests/autotests/test_task4.py
+++ tests/autotests/test_task4.py
@@ -6,7 +6,7 @@ import random
 from copy import deepcopy
 import pytest
 from regex_constants import REGEXES
-from helper import generate_rnd_start_and_final
+from helper import generate_rnd_start_and_final, rpq_dict_to_set
 from fixtures import graph
 
 # Fix import statements in try block to run tests
@@ -30,15 +30,11 @@ class TestReachability:
             graph_to_nfa(deepcopy(graph), deepcopy(start_nodes), deepcopy(final_nodes))
         )
         constraint_fa = FiniteAutomaton(regex_to_dfa(query))
-        reachable: dict = reachability_with_constraints(
-            deepcopy(fa), deepcopy(constraint_fa)
+        reachable = rpq_dict_to_set(
+            reachability_with_constraints(deepcopy(fa), deepcopy(constraint_fa))
         )
-        reachable = {k: v for k, v in reachable.items() if len(v) != 0}
         ends = paths_ends(
             deepcopy(graph), deepcopy(start_nodes), deepcopy(final_nodes), query
         )
 
-        assert len(set(reachable.keys())) == len(set(map(lambda x: x[0], ends)))
-
-        for start, final in ends:
-            assert start in reachable.keys() and final in reachable[start]
+        assert set(ends) == reachable
