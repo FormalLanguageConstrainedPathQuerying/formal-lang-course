7208040f0929f2a7a6f0f0638fe16a7fe05c54e4
Efim Kubishkin <efimkub@mail.ru>
2024-05-21 12:18:34 +0300

    Rework Program class

--- tests/autotests/to_program_parser.py
+++ tests/autotests/to_program_parser.py
@@ -5,8 +5,7 @@ import itertools
 import pyformlang as pl
 from pyformlang.cfg import Variable, Terminal
 import networkx as nx
-
-LABEL = "label"
+from constants import LABEL
 
 
 class FreshVar:
@@ -18,25 +17,150 @@ class FreshVar:
         return f"{var}{cls.var_counter}"
 
 
+def _nonterminal_to_string(nonterminal: Variable) -> str:
+    return nonterminal.to_text().lower()
+
+
+def _terminal_to_string(terminal: Terminal) -> str:
+    """
+    convert terminal symbol into char
+    :param terminal: terminal symbol
+    :return:
+    """
+    terminal_s = terminal.to_text().lower()
+    if len(terminal_s) == 1:
+        return f'"{terminal_s}"'
+    res = ""
+    for key, group in itertools.groupby(terminal_s):
+        dot = " . " if res != "" else ""
+        if len(group) > 1:
+            res += f'{dot}"{key}"^[{len(group)}]'
+        else:
+            res += f'{dot}"{key}"'
+    return res
+
+
+class GraphProgram:
+    def __init__(self, graph: nx.MultiDiGraph):
+        self.graph = graph.copy()
+        self.name = (
+            FreshVar.generate_fresh(graph.name)
+            if graph.name != ""
+            else FreshVar.generate_fresh("g")
+        )
+
+    def __str__(self):
+        program = f"let {self.graph.name} is graph"
+        for node_from, node_to, data in self.graph.edges(data=True):
+            program += f'\nadd edge ({node_from}, "{data[LABEL]}", {node_to}) to {self.graph.name}'
+        return program
+
+
+class GrammarProgram:
+    nonterminal_names = {}
+    EPS = '"a"^[0]'
+
+    def __init__(self, cfg: pl.cfg.CFG):
+        self.grammar = copy(cfg)
+        for production in cfg.productions:
+            if production.head not in self.nonterminal_names.keys():
+                self.nonterminal_names[production.head] = FreshVar.generate_fresh(
+                    _nonterminal_to_string(production.head)
+                )
+        self.start_nonterminal_name = self.nonterminal_names[cfg.start_symbol]
+
+    def _object_to_string(self, cfg_object: Variable | Terminal) -> str:
+        """
+        convert nonterminal or terminal symbol into program representation
+        :param cfg_object: terminal or nonterminal
+        :return: an object view of "x", if object is terminal or x, if object is nonterminal
+        """
+        if isinstance(cfg_object, Variable):
+            return self.nonterminal_names[cfg_object]
+        return _terminal_to_string(cfg_object)
+
+    def _objs_to_expr(self, objects: list[pl.cfg.production.CFGObject]) -> str:
+        if len(objects) == 0:
+            return self.EPS
+        return functools.reduce(
+            lambda acc, obj: f"{acc}{' . ' if acc != '' else ''}{self._object_to_string(obj)}",
+            objects,
+            "",
+        )
+
+    def _objs_alts(self, objects: list[list[pl.cfg.production.CFGObject]]) -> str:
+        return functools.reduce(
+            lambda acc, objs: f"{acc}{' | ' if acc != '' else ''}{self._objs_to_expr(objs)}",
+            objects,
+            "",
+        )
+
+    def __str__(self) -> str:
+        res = ""
+        vars_dict: dict[pl.cfg.Variable, list[list[pl.cfg.production.CFGObject]]] = {}
+        for production in self.grammar.productions:
+            head = production.head
+            body = production.body
+            if head in vars_dict.keys():
+                vars_dict[head].append(body)
+            else:
+                vars_dict[head] = [body]
+        for nonterminal in vars_dict.keys():
+            res += f"\nlet {self.nonterminal_names[nonterminal]} = {self._objs_alts(vars_dict[nonterminal])}"
+        return res
+
+
+class QueryProgram:
+    def __init__(
+        self,
+        graph_program: GraphProgram,
+        grammar_program: GrammarProgram,
+        start_nodes: set[int],
+        final_nodes: set[int] = None,
+    ):
+        if final_nodes is None:
+            final_nodes = set()
+        self.graph_program = graph_program
+        self.grammar_program = grammar_program
+        self.result_name = FreshVar.generate_fresh("r")
+        self.start_nodes = start_nodes
+        self.final_nodes = final_nodes
+
+    def __str__(self) -> str:
+        query_name = self.grammar_program.start_nonterminal_name
+        start_set_expr = (
+            "["
+            + functools.reduce(
+                lambda acc, x: f"{acc}{', ' if acc != '' else ''}{str(x)}",
+                self.start_nodes,
+                "",
+            )
+            + "]"
+        )
+        if len(self.final_nodes) == 0:
+            return (
+                f"\nlet {self.result_name} = for v in {start_set_expr} return u, v where u reachable from v in "
+                f"{self.graph_program.name} by {query_name}"
+            )
+        final_set_expr = (
+            "["
+            + functools.reduce(
+                lambda acc, x: f"{acc}{', ' if acc != '' else ''}{str(x)}",
+                self.final_nodes,
+                "",
+            )
+            + "]"
+        )
+        return (
+            f"\nlet {self.result_name} = for v in {start_set_expr} for u in {final_set_expr} return u, v where u "
+            f"reachable from v in {self.graph_program.name} by {query_name}"
+        )
+
+
 class Program:
     EPS = '"a"^[0]'
     nonterminal_names = {}
     result_name = ""
-
-    @staticmethod
-    def _nonterminal_to_string(nonterminal: Variable) -> str:
-        return nonterminal.to_text().lower()
-
-    @staticmethod
-    def _terminal_to_string(terminal: Terminal) -> str:
-        terminal_s = terminal.to_text().lower()
-        if len(terminal_s) == 1:
-            return f'"{terminal_s}"'
-        res = ""
-        for key, group in itertools.groupby(terminal_s):
-            dot = " . " if res != "" else ""
-            res += f'{dot}"{key}"^[{len(group)}]'
-        return res
 
     def __init__(
         self,
@@ -58,76 +182,8 @@ class Program:
         for production in cfg.productions:
             if production.head not in self.nonterminal_names.keys():
                 self.nonterminal_names[production.head] = FreshVar.generate_fresh(
-                    self._nonterminal_to_string(production.head)
+                    _nonterminal_to_string(production.head)
                 )
-
-    def _graph_to_program(self) -> str:
-        program = f"let {self.graph.name} is graph"
-        for node_from, node_to, data in self.graph.edges(data=True):
-            program += f'\nadd edge ({node_from}, "{data[LABEL]}", {node_to}) to {self.graph.name}'
-        return program
-
-    def _object_to_string(self, cfg_object: Variable | Terminal) -> str:
-        if isinstance(cfg_object, Variable):
-            return self.nonterminal_names[cfg_object]
-        return self._terminal_to_string(cfg_object)
-
-    def _objs_to_expr(self, objects: list[pl.cfg.production.CFGObject]) -> str:
-        if len(objects) == 0:
-            return self.EPS
-        return functools.reduce(
-            lambda acc, obj: f"{acc}{' . ' if acc != '' else ''}{self._object_to_string(obj)}",
-            objects,
-            "",
-        )
-
-    def _objs_alts(self, objects: list[list[pl.cfg.production.CFGObject]]) -> str:
-        return functools.reduce(
-            lambda acc, objs: f"{acc}{' | ' if acc != '' else ''}{self._objs_to_expr(objs)}",
-            objects,
-            "",
-        )
-
-    def _cfg_to_program(self) -> str:
-        res = ""
-        vars_dict: dict[pl.cfg.Variable, list[list[pl.cfg.production.CFGObject]]] = {}
-        for production in self.grammar.productions:
-            head = production.head
-            body = production.body
-            if head in vars_dict.keys():
-                vars_dict[head].append(body)
-            else:
-                vars_dict[head] = [body]
-        for nonterminal in vars_dict.keys():
-            res += f"\nlet {self.nonterminal_names[nonterminal]} = {self._objs_alts(vars_dict[nonterminal])}"
-        return res
-
-    def _query_to_program(self) -> str:
-        query_name = self.nonterminal_names[self.grammar.start_symbol]
-        start_set_expr = (
-            "["
-            + functools.reduce(
-                lambda acc, x: f"{acc}{', ' if acc != '' else ''}{str(x)}",
-                self.start_nodes,
-                "",
-            )
-            + "]"
-        )
-        if len(self.final_nodes) == 0:
-            return f"\nlet {self.result_name} = for v in {start_set_expr} return u, v where u reachable from v in {self.graph.name} by {query_name}"
-        final_set_expr = (
-            "["
-            + functools.reduce(
-                lambda acc, x: f"{acc}{', ' if acc != '' else ''}{str(x)}",
-                self.final_nodes,
-                "",
-            )
-            + "]"
-        )
-        return (
-            f"\nlet {self.result_name} = for v in {start_set_expr} for u in {final_set_expr} return u, v where u reachable from v in {self.graph.name} "
-            f"by {query_name}"
-        )
 
     def __str__(self):
         program = ""
