d174b25a9fb6f51af4d687d97123c27642b1be77
AndrewPolyakov1 <mr.dijoy@gmail.com>
2024-05-21 17:23:45 +0300

    [Andrew ] Parallel run for Workflows

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -4,7 +4,9 @@ import shared
 
 def main():
     shared.configure_python_path()
-    subprocess.check_call(["python", "-m", "pytest", "-vv", "-s", shared.TESTS])
+    subprocess.check_call(
+        ["python", "-m", "pytest", "-vv", "-n", "8", "-s", shared.TESTS]
+    )
 
 
 if __name__ == "__main__":
