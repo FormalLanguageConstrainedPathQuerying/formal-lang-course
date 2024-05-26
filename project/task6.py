934b442bc7d8fb3aafe39719c7c2a528eeb48ee6
AndrewPolyakov1 <mr.dijoy@gmail.com>
2024-05-26 16:04:41 +0300

    [Andrew ] Style fix

--- project/task6.py
+++ project/task6.py
@@ -7,15 +7,11 @@ import networkx as nx
 
 
 def cfg_to_weak_normal_form(cfg: pyformlang.cfg.CFG) -> pyformlang.cfg.CFG:
-    grammatical = (cfg.
-                   eliminate_unit_productions().
-                   remove_useless_symbols())
-    _new_productions = (grammatical.
-    _decompose_productions(
+    grammatical = cfg.eliminate_unit_productions().remove_useless_symbols()
+    _new_productions = grammatical._decompose_productions(
         grammatical._get_productions_with_only_single_terminals()
-    ))
-    return CFG(start_symbol=grammatical.start_symbol,
-               productions=_new_productions)
+    )
+    return CFG(start_symbol=grammatical.start_symbol, productions=_new_productions)
 
 
 def cfg_from_file(path: str) -> CFG:
@@ -24,10 +20,10 @@ def cfg_from_file(path: str) -> CFG:
 
 
 def cfpq_with_hellings(
-        cfg: pyformlang.cfg.CFG,
-        graph: nx.DiGraph,
-        start_nodes: Set[int] = None,
-        final_nodes: Set[int] = None,
+    cfg: pyformlang.cfg.CFG,
+    graph: nx.DiGraph,
+    start_nodes: Set[int] = None,
+    final_nodes: Set[int] = None,
 ) -> Set[Tuple[int, int]]:
     if start_nodes is None:
         start_nodes = graph.nodes
@@ -40,21 +36,16 @@ def cfpq_with_hellings(
     p_3 = {}
 
     for _production in grammatics.productions:
-        if (len(_production.body) == 1 and
-                isinstance(_production.body[0], Terminal)):
-            p_1.setdefault(
-                _production.head, set()
-            ).add(_production.body[0])
-        elif (len(_production.body) == 1 and
-              isinstance(_production.body[0], Epsilon)):
+        if len(_production.body) == 1 and isinstance(_production.body[0], Terminal):
+            p_1.setdefault(_production.head, set()).add(_production.body[0])
+        elif len(_production.body) == 1 and isinstance(_production.body[0], Epsilon):
             p_2.add(_production.body[0])
         elif len(_production.body) == 2:
-            (p_3.setdefault(
-                _production.head,
-                set()
+            (
+                p_3.setdefault(_production.head, set()).add(
+                    (_production.body[0], _production.body[1])
+                )
             )
-             .add((_production.body[0], _production.body[1]))
-             )
 
     result = {(n_i, vertice, vertice) for n_i in p_2 for vertice in graph.nodes}
     result |= {
@@ -81,11 +72,13 @@ def cfpq_with_hellings(
     answer = {
         (v_i, u_i)
         for (n_i, v_i, u_i) in result
-        if v_i in start_nodes and u_i in final_nodes and Variable(n_i) == cfg.start_symbol
+        if v_i in start_nodes
+        and u_i in final_nodes
+        and Variable(n_i) == cfg.start_symbol
     }
 
     return answer
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
