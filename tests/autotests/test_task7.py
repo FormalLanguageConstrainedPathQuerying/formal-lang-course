46396cebb6a95a48cbd7efdee5e04c2fa135d4d6
Denis Shkandyuk <57489789+Rollcorn@users.noreply.github.com>
2024-06-20 16:46:00 +0300

    solved task 7 (#9)

--- tests/autotests/test_task7.py
+++ tests/autotests/test_task7.py
@@ -11,11 +11,11 @@ from fixtures import graph
 
 # Fix import statements in try block to run tests
 try:
-    from project.task2 import graph_to_nfa, regex_to_dfa
-    from project.task3 import FiniteAutomaton
-    from project.task4 import reachability_with_constraints
-    from project.task7 import cfpq_with_matrix
-    from project.task6 import cfpq_with_hellings
+    from project.task2.fa_builders import graph_to_nfa, regex_to_dfa
+    from project.task3.finite_automaton import FiniteAutomaton
+    from project.task4.reachability import reachability_with_constraints
+    from project.task6.grammar_transform import cfpq_with_hellings
+    from project.task7.cfpq_with_matrix import cfpq_with_matrix
 except ImportError:
     pytestmark = pytest.mark.skip("Task 7 is not ready to test!")
 
