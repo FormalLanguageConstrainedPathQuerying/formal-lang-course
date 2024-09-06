6cc581b372c2eb4bdfab04c8fa82799ec6699bf4
Lesh79 <Temerlan.axmetov.75@mail.ru>
2024-09-06 20:33:29 +0300

    Add a graphics module and tests for it

---
+++ project/graph_module.py
@@ -0,0 +1,36 @@
+from typing import Tuple
+
+import cfpq_data
+from networkx.drawing import nx_pydot
+import networkx as nx
+
+
+class Graph:
+    @staticmethod
+    def load_graph(name: str) -> nx.MultiDiGraph:
+        try:
+            graph_path = cfpq_data.download(name)
+            return cfpq_data.graph_from_csv(graph_path)
+        except FileNotFoundError:
+            raise FileNotFoundError
+
+    @staticmethod
+    def graph_info(name: str):
+        graph = Graph.load_graph(name)
+        return graph.number_of_nodes(), graph.number_of_edges(), cfpq_data.get_sorted_labels(graph)
+
+    @staticmethod
+    def create_labeled_graph(count_of_nodes: int,
+                             count_of_edges: int,
+                             label: Tuple[str, str],
+                             path: str) -> None:
+        graph = cfpq_data.labeled_two_cycles_graph(n=count_of_nodes, m=count_of_edges, labels=label)
+        Graph.save_graph(graph, path)
+
+    @staticmethod
+    def save_graph(graph: nx.MultiDiGraph, path: str) -> None:
+        grph = nx_pydot.to_pydot(graph)
+        grph.write(path)
+
+
+Graph.create_labeled_graph(5, 12, ("11", "12"), "../tests/test_create_graph_expected.dot")
---
+++ tests/test_graph_module.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+from networkx.drawing import nx_pydot
+
+from project.graph_module import Graph
+
+
+class TestGraph:
+
+    def test_name_graph(self):
+        with pytest.raises(FileNotFoundError):
+            Graph.load_graph("Lesh79")
+
+    def test_graph_info_bzip(self):
+        graph_info_bzip = (632, 556, ['d', 'a'])
+        assert Graph.graph_info("bzip") == graph_info_bzip
+
+    def test_graph_info_pizza(self):
+        graph_info_pizza = (671, 1980,
+                            ['disjointWith', 'type', 'subClassOf', 'onProperty', 'first', 'rest', 'someValuesFrom',
+                             'label', 'allValuesFrom', 'comment', 'unionOf', 'equivalentClass', 'intersectionOf',
+                             'range', 'domain', 'hasValue', 'distinctMembers', 'subPropertyOf', 'complementOf',
+                             'inverseOf', 'versionInfo', 'minCardinality', 'oneOf'])
+        assert Graph.graph_info("pizza") == graph_info_pizza
+
+    def test_graph_create(self):
+        path_to_file: str = "test_create.dot"
+        Graph.create_labeled_graph(5, 12, ("11", "12"), path_to_file)
+        graph = nx_pydot.read_dot(path_to_file)
+        os.remove(path_to_file)
+        expected_graph = nx_pydot.read_dot("test_create_graph_expected.dot")
+        assert graph.graph == expected_graph.graph
