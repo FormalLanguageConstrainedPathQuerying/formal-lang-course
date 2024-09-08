0f260a5ef75891739a0f7916e8dc6958fc5adc91
Andrei <therain.i@yahoo.com>
2024-09-08 23:20:30 +0300

    feat(graph_tools): impl `build_save_2cycles_graph`

--- project/graph_tools.py
+++ project/graph_tools.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 import cfpq_data
+from networkx import nx_pydot
 
 
 @dataclass
@@ -17,3 +18,19 @@ def get_graph_metadata(graph_name: str) -> GraphMetadata:
         edge_count=graph.number_of_edges(),
         edge_labels={lbl for _, _, lbl in graph.edges(data="label")},
     )
+
+
+def build_save_2cycles_graph(
+    cycle1_nodes: int,
+    cycle1_labels: str,
+    cycle2_nodes: int,
+    cycle2_labels: str,
+    output_path: str,
+) -> None:
+    """Constructs graph with two cycles and saves it in a DOT file"""
+    graph = cfpq_data.labeled_two_cycles_graph(
+        n=cycle1_nodes,
+        m=cycle2_nodes,
+        labels=(cycle1_labels, cycle2_labels),
+    )
+    nx_pydot.to_pydot(graph).write_raw(output_path)
