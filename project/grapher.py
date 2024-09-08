7ca0ef9d2d8d445fe491cb0e3f24ae68330dc626
d-zaytsev <dzaytsev308@gmail.com>
2024-09-08 11:00:33 +0300

    Fix: make fields private

--- project/grapher.py
+++ project/grapher.py
@@ -4,31 +4,26 @@ import networkx as nx
 
 
 class GraphData:
-    def __init__(self):
-        self.nodes_count = 0
-        self.edges_count = 0
-        self.labels = set()
-
     # Function 1 from task 1
     def __init__(self, name: str):
         csv_path = cfpq.download(name)
         graph = cfpq.graph_from_csv(csv_path)
 
-        self.nodes_count = graph.number_of_nodes
-        self.edges_count = graph.number_of_edges
-        self.labels = cfpq.get_sorted_labels(graph)
+        self.__nodes_count = graph.number_of_nodes()
+        self.__edges_count = graph.number_of_edges()
+        self.__labels = cfpq.get_sorted_labels(graph)
 
     @property
-    def vertices(self):
-        return self.nodes_count
+    def nodes(self):
+        return self.__nodes_count
 
     @property
     def edges(self):
-        return self.edges_count
+        return self.__edges_count
 
     @property
     def edge_labels(self):
-        return list(self.labels)
+        return list(self.__labels)
 
 
 # Graph saver
