efa253bd882a5cb2ba7097b28360850dccd97d66
ksenmel <ksemelashenko@gmail.com>
2024-09-15 21:36:45 +0300

    add task2 utils and tests

---
+++ project/task2_fa.py
@@ -0,0 +1,39 @@
+from networkx import MultiDiGraph
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+    EpsilonNFA,
+    State,
+)
+from pyformlang.regular_expression import Regex
+from typing import Set
+
+
+# function for constructing a minimal DFA for a given regular expression
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    enfa = Regex(regex).to_epsilon_nfa()
+    dfa = enfa.remove_epsilon_transitions().to_deterministic().minimize()
+
+    return dfa
+
+
+def graph_to_nfa(
+    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
+) -> NondeterministicFiniteAutomaton:
+    nodes = graph.nodes
+
+    # https://pyformlang.readthedocs.io/en/latest/modules/finite_automaton.html#pyformlang.finite_automaton.FiniteAutomaton.from_networkx
+    enfa = EpsilonNFA.from_networkx(graph)
+    nfa = enfa.remove_epsilon_transitions()
+
+    if start_states:
+        nfa.add_start_state(State(i) for i in start_states)
+    else:
+        nfa.add_start_state(State(i) for i in nodes)
+
+    if final_states:
+        nfa.add_final_state(State(i) for i in final_states)
+    else:
+        nfa.add_final_state(State(i) for i in nodes)
+
+    return nfa
---
+++ tests/test_task2_fa.py
@@ -0,0 +1,21 @@
+from pyformlang.finite_automaton import Symbol
+
+from project.task2_fa import regex_to_dfa
+
+
+class TestRegexToDfa:
+    def test_regex_to_dfa_1(self):
+        dfa = regex_to_dfa("a|b|c*")
+
+        assert dfa.accepts("a")
+        assert dfa.accepts("ccc")
+        assert dfa.accepts("")
+        assert not dfa.accepts("abab")
+        assert not dfa.accepts("aaaa")
+
+    def test_regex_to_dfa_2(self):
+        dfa = regex_to_dfa("abc|d")
+
+        assert dfa.accepts([Symbol("abc")])
+        assert dfa.accepts([Symbol("d")])
+        assert not dfa.accepts([Symbol("b")])
