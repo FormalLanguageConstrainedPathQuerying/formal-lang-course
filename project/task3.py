3d3fdf5d95446a7c71b13ded64caa605211088d5
d-zaytsev <dzaytsev308@gmail.com>
2024-09-20 22:41:39 +0300

    Add self.state value

--- project/task3.py
+++ project/task3.py
@@ -9,7 +9,6 @@ from numpy.typing import NDArray
 from scipy.sparse import csr_array, kron
 from typing import Iterable
 from networkx import MultiDiGraph
-from task2 import regex_to_dfa, graph_to_nfa
 
 
 class AdjacencyMatrixFA:
@@ -18,8 +17,9 @@ class AdjacencyMatrixFA:
         fa: (NondeterministicFiniteAutomaton | DeterministicFiniteAutomaton | None),
     ):
         self.states_count: int = 0
-        self.start_states = set()
-        self.final_states = set()
+        self.states: list[State] = set()
+        self.start_states_is: set[int] = set()  # indexes of states
+        self.final_states_is: set[int] = set()  # indexes of states
         self.sparse_matrices: dict[Symbol, csr_array] = {}
 
         if fa is None:
@@ -31,20 +31,28 @@ class AdjacencyMatrixFA:
         transitions_dict: dict = fa.to_dict()
         transitions: list[tuple] = list(transitions_dict.items())
 
-        # set nodes
-        self.start_states = set(int(x._value) for x in fa.start_states)
-        self.final_states = set(int(x._value) for x in fa.final_states)
+        # set states
+        self.states = list(fa.states)
+        self.states_count = len(fa.states)
+
+        for i in range(self.states_count):
+            state: State = self.states[i]
+
+            if state in fa.start_states:
+                self.start_states_is.add(i)
+            elif state in fa.final_states:
+                self.final_states_is.add(i)
 
         # dictionary for bool decomposition
         matrices_dict: dict[Symbol, NDArray[np.bool_]] = {}
 
-        self.states_count = len(fa.states)
         matrix_shape = (self.states_count, self.states_count)
         # ^ there may be an error, since there may be states that are not in order
 
         for source_node, dests in transitions:
             # (source_node, {sym: dest_node , ...})
-            source_node: int = int(source_node._value)
+            # index of source_node
+            source_node_i: int = self.states.index(source_node)
 
             for sym, dest in dests.items():
                 # DFA: (sym, dest_node)
@@ -55,16 +63,17 @@ class AdjacencyMatrixFA:
                     matrices_dict[sym] = np.zeros(shape=matrix_shape, dtype=np.bool_)
 
                 if isDeterministic:
-                    dest_node: int = dest._value
+                    dest_node_i: int = self.states.index(dest)
 
-                    matrices_dict[sym][source_node, dest_node] = True
+                    matrices_dict[sym][source_node_i, dest_node_i] = True
 
                 else:
-                    dest_nodes: dict[int] = dest
+                    # different transitions from one state
+                    dest_nodes: set[State] = dest
 
                     for dest_node in dest_nodes:
-                        dest_node = dest_node._value
-                        matrices_dict[sym][source_node, dest_node] = True
+                        dest_node_i: int = self.states.index(dest_node)
+                        matrices_dict[sym][source_node_i, dest_node_i] = True
 
         # transform to sparse matrices
         for key in list(matrices_dict.keys()):
@@ -75,8 +84,8 @@ class AdjacencyMatrixFA:
         confs: list[(int, Iterable[Symbol])] = []
 
         # add start configurations
-        for start_state in self.start_states:
-            confs.append((start_state, word))
+        for start_state_i in self.start_states_is:
+            confs.append((start_state_i, word))  # indexes of states + words
 
         while True:
             # if we can't find accept state
@@ -85,11 +94,11 @@ class AdjacencyMatrixFA:
 
             cur_conf = confs.pop()
 
-            cur_state = cur_conf[0]
+            cur_state_i = cur_conf[0]
             cur_word = cur_conf[1]
 
             # accept word
-            if (not cur_word) and (cur_state in self.final_states):
+            if (not cur_word) and (cur_state_i in self.final_states_is):
                 return True
 
             cur_sym = cur_word[0]
@@ -102,14 +111,14 @@ class AdjacencyMatrixFA:
 
                 # add new confs (by matrix row)
                 row_states = range(matrix.shape[0])
-                next_states = [
+                next_states_is = [
                     next_state
                     for next_state in row_states
-                    if matrix[cur_state, next_state]
+                    if matrix[cur_state_i, next_state]
                 ]
 
-                for next_state in next_states:
-                    confs.append((next_state, cur_word[1:]))
+                for next_state_i in next_states_is:
+                    confs.append((next_state_i, cur_word[1:]))
 
     def transitive_сlosure(self) -> NDArray[np.bool_]:
         adj_matrix: NDArray[np.bool_] = np.zeros(
@@ -119,7 +128,7 @@ class AdjacencyMatrixFA:
         for matrix in self.sparse_matrices.values():
             adj_matrix = adj_matrix | matrix.toarray()
 
-        for start_node in self.start_states:
+        for start_node in self.start_states_is:
             adj_matrix[start_node, start_node] = True
 
         for k in range(self.states_count):
@@ -134,8 +143,8 @@ class AdjacencyMatrixFA:
     def is_empty(self) -> bool:
         matrix: NDArray[np.bool_] = self.transitive_сlosure()
 
-        for start_node in self.start_states:
-            for final_node in self.final_states:
+        for start_node in self.start_states_is:
+            for final_node in self.final_states_is:
                 if start_node != final_node and matrix[start_node, final_node]:
                     return False  # if FA graph have path from start to finish
 
@@ -161,10 +170,10 @@ def intersect_automata(
         for s2 in range(a2.states_count):
             new_index = a2.states_count * s1 + s2
 
-            if (s1 in a1.start_states) and (s2 in a2.start_states):
-                new_a.start_states.add(new_index)
-            if (s1 in a1.final_states) and (s2 in a2.final_states):
-                new_a.final_states.add(new_index)
+            if (s1 in a1.start_states_is) and (s2 in a2.start_states_is):
+                new_a.start_states_is.add(new_index)
+            if (s1 in a1.final_states_is) and (s2 in a2.final_states_is):
+                new_a.final_states_is.add(new_index)
 
     return new_a
 
@@ -188,8 +197,8 @@ def tensor_based_rpq(
 
     for graph_start in start_nodes:
         for graph_final in final_nodes:
-            for regex_start in mfa2.start_states:
-                for regex_final in mfa2.final_states:
+            for regex_start in mfa2.start_states_is:
+                for regex_final in mfa2.final_states_is:
                     start_index = mfa2.states_count * graph_start + regex_start
                     final_index = mfa2.states_count * graph_final + regex_final
 
@@ -197,9 +206,3 @@ def tensor_based_rpq(
                         pairs.add((graph_start, graph_final))
 
     return list(pairs)
-
-
-fa = NondeterministicFiniteAutomaton()
-fa.add_transitions([(0, "a", 1), (1, "c", 2), (1, "a", 0)])
-
-tensor_based_rpq(fa.to_networkx(), set([0]), set([2]), "a*.c")
