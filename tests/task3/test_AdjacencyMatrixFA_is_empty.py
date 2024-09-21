589d6f1d6c2a779130f970ffbfe918ab2ce51bef
d-zaytsev <dzaytsev308@gmail.com>
2024-09-21 18:59:16 +0300

    remove incorrect test

--- tests/task3/test_AdjacencyMatrixFA_is_empty.py
+++ tests/task3/test_AdjacencyMatrixFA_is_empty.py
@@ -1,5 +1,5 @@
 from project.task3 import AdjacencyMatrixFA
-from pyformlang.finite_automaton import NondeterministicFiniteAutomaton, Symbol
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
 
 
 def test_not_empty_fa():
@@ -12,17 +12,7 @@ def test_not_empty_fa():
     assert not amf.is_empty()
 
 
-def test_empty_fa_1():
-    fa = NondeterministicFiniteAutomaton()
-    fa.add_transitions([(0, "a", 1), (0, "b", 2), (1, "c", 2), (1, "a", 0)])
-    fa.add_start_state(2)
-    fa.add_final_state(2)
-
-    amf = AdjacencyMatrixFA(fa)
-    assert amf.is_empty()
-
-
-def test_empty_fa_2():
+def test_empty_fa():
     fa = NondeterministicFiniteAutomaton()
     fa.add_transitions([(0, "a", 1), (0, "b", 2), (1, "c", 2), (1, "a", 0)])
     fa.add_start_state(2)
