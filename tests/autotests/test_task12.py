96c4b7a55db1012ef94c2e110998b1670df911ed
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2025-01-27 21:59:51 +0300

    fix: add fix for test (by d-zaytsev)

--- tests/autotests/test_task12.py
+++ tests/autotests/test_task12.py
@@ -17,8 +17,8 @@ from helper import generate_rnd_start_and_final, generate_rnd_dense_graph
 from constants import LABELS
 
 try:
-    from project.task7 import matrix_based_cfpq
-    from project.task12 import typing_program, exec_program
+    from project.hw7.matrix_cfpq import matrix_based_cfpq
+    from project.hw12.run_interp import typing_program, exec_program
 except ImportError:
     pytestmark = pytest.mark.skip("Task 12 is not ready to test!")
 
@@ -77,7 +77,7 @@ class TestProgramInterpreter:
             query_full_program = query.full_program()
             assert typing_program(deepcopy(query_full_program))
             separate_res = exec_program(deepcopy(query_full_program))
-            assert separate_res == res
+            assert separate_res[var] == res
             assert res == matrix_based_cfpq(
                 query.get_grammar(),
                 query.get_graph(),
