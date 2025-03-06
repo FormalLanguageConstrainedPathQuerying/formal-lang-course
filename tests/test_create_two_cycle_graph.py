28013fd738082cb43654d41db926221ee28b0c19
kadoc <kadoch.daniil@gmail.com>
2025-03-06 03:12:37 +0300

    Fixes for CI. Modifies paths for resources.

--- tests/test_create_two_cycle_graph.py
+++ tests/test_create_two_cycle_graph.py
@@ -1,5 +1,6 @@
 from project.graph import create_two_cycle_graph
 import pytest
+import filecmp
 import networkx
 import cfpq_data
 import pydot
@@ -15,12 +16,14 @@ def test_create_two_cycle_graph():
     create_two_cycle_graph(
         nodes_left, nodes_right, (first_label, second_label), name
     )
-
-    with open("resources/two_cycle_graph.dot", "r") as resource_graph:
+    assert filecmp.cmp("tests/resources/two_cycle_graph.dot", "test_graph.dot", shallow=False)
+    current_dir = os.getcwd()
+    print("Current directory:", current_dir)
+    with open("tests/resources/two_cycle_graph.dot", "r") as resource_graph:
         with open(f"{name}") as output_graph:
             assert resource_graph.read() == output_graph.read()
 
-    pydot_graph = pydot.graph_from_dot_file("resources/two_cycle_graph.dot")[0]
+    pydot_graph = pydot.graph_from_dot_file("tests/resources/two_cycle_graph.dot")[0]
     resource_graph = networkx.nx_pydot.from_pydot(pydot_graph)
 
     pydot_graph = pydot.graph_from_dot_file(f"{name}")[0]
