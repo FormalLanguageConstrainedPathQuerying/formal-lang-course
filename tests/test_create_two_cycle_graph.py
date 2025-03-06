11cd71c3221f9f579987bcbe541cab5527e4819a
kadoc <kadoch.daniil@gmail.com>
2025-03-06 13:52:06 +0300

    Removes unnecessary checks and imports

--- tests/test_create_two_cycle_graph.py
+++ tests/test_create_two_cycle_graph.py
@@ -1,8 +1,5 @@
 from project.task1 import create_two_cycle_graph
 import filecmp
-import networkx
-import cfpq_data
-import pydot
 import os
 
 
@@ -17,19 +14,9 @@ def test_create_two_cycle_graph():
         nodes_left, nodes_right, (first_label, second_label), name
     )
     assert filecmp.cmp("tests/resources/two_cycle_graph.dot", "test_graph.dot", shallow=False)
-    current_dir = os.getcwd()
-    print("Current directory:", current_dir)
+
     with open("tests/resources/two_cycle_graph.dot", "r") as resource_graph:
         with open(f"{name}") as output_graph:
             assert resource_graph.read() == output_graph.read()
 
-    pydot_graph = pydot.graph_from_dot_file("tests/resources/two_cycle_graph.dot")[0]
-    resource_graph = networkx.nx_pydot.from_pydot(pydot_graph)
-
-    pydot_graph = pydot.graph_from_dot_file(f"{name}")[0]
-    output_graph = networkx.nx_pydot.from_pydot(pydot_graph)
-
-    assert resource_graph.number_of_nodes() == output_graph.number_of_nodes()
-    assert resource_graph.number_of_edges() == output_graph.number_of_edges()
-    assert cfpq_data.get_sorted_labels(resource_graph) == cfpq_data.get_sorted_labels(output_graph)
     os.remove(name)
