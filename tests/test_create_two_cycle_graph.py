cd92f0797dc8d4886b8c0f1bc4801e97c26f19bb
kadoc <kadoch.daniil@gmail.com>
2025-03-08 14:41:28 +0300

    Fixes paths for test_create_two_cycle_graph test for CI

--- tests/test_create_two_cycle_graph.py
+++ tests/test_create_two_cycle_graph.py
@@ -13,9 +13,9 @@ def test_create_two_cycle_graph():
     create_two_cycle_graph(
         nodes_left, nodes_right, (first_label, second_label), name
     )
-    assert filecmp.cmp("resources/two_cycle_graph.dot", "test_graph.dot", shallow=False)
+    assert filecmp.cmp("tests/resources/two_cycle_graph.dot", "test_graph.dot", shallow=False)
 
-    with open("resources/two_cycle_graph.dot", "r") as resource_graph:
+    with open("tests/resources/two_cycle_graph.dot", "r") as resource_graph:
         with open(f"{name}") as output_graph:
             assert resource_graph.read() == output_graph.read()
 
