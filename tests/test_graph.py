3436d5652df6fa1d0330a85599e6e295a444e235
Nikita Shchutskii <nshchutskiy@gmail.com>
2025-09-07 23:59:30 +0300

    repair tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,22 +1,21 @@
 import project.graph as graph
 import cfpq_data.dataset.data as ds
 import pytest
-import networkx
 import pydot
 
 
 class TestCrAndSave2CycGraph:
     def test_read_after_write(self):
         graph.create_and_safe_2cycle_graph(10, 10, ("a", "b"), "ex.dot")
-        g = networkx.drawing.nx_pydot.from_pydot(pydot.graph_from_dot_file("ex.dot"))
-        assert g.number_of_nodes == 21
-        assert g.number_of_edges == 22
-        assert {edge.label for edge in g.edges} == {"a", "b"}
+        g = pydot.graph_from_dot_file("ex.dot")
+        assert len(g.get_node_list()) == 21
+        assert len(g.get_edge_list) == 22
+        assert {edge.label for edge in g.get_edge_list} == {"a", "b"}
 
 
 class TestGraphInfo:
     def test_nonneg_graph_info(self):
-        infos = (ds.DATASET[0:3]).map(graph.Info)
+        infos = map(graph.Info, ds.DATASET[0:3])
         for info in infos:
             assert info.number_of_nodes >= 0
             assert info.number_of_edges >= 0
