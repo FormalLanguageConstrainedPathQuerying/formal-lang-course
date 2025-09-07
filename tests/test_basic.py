c685107afa580e707e8fd6c8a8336aedd336e977
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-09-07 17:41:10 +0300

    test: add basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,8 @@
-import pytest  # noqa: F401
-import project  # on import will print something from __init__ file # noqa: F401
-
+from project.graph_info import *
+import networkx as nx
+import os
+import pytest
+import pydot
 
 def setup_module(module):
     print("basic setup module")
@@ -10,9 +12,26 @@ def teardown_module(module):
     print("basic teardown module")
 
 
-def test_1():
-    assert 1 + 1 == 2
+def test_get_graph_info_wc():
+    info = get_graph_info("wc")
+    assert info.node_num == 332
+    assert info.edge_num == 269
+    assert info.labels == ["d", "a"]
 
 
-def test_2():
-    assert "1" + "1" == "11"
+def test_get_graph_info_notexist():
+    with pytest.raises(FileNotFoundError):
+        get_graph_info("Pridnestrovie")
+
+
+def test_save_graph():
+    n, m, labels = 3, 9, ["h", "m"]
+    filename = "tests/out_test_save_graph.dot"
+
+    save_two_cycle_graph(n, m, labels, filename)
+    graph: nx.MultiDiGraph = nx.drawing.nx_pydot.read_dot(filename)
+
+    assert graph.number_of_nodes() == n + m + 1
+    assert set(cfpq_data.get_sorted_labels(graph)) == set(labels)
+
+
